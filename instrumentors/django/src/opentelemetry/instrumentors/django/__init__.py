3a7efe1b810757ee6d8cc57abdfe493fb0c19126
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-03-28 00:34:04 -0600

    Add Django instrumentor

---
+++ instrumentors/django/src/opentelemetry/instrumentors/django/__init__.py
@@ -0,0 +1,71 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from logging import getLogger
+
+from django import VERSION
+from django.conf import settings
+
+from opentelemetry.auto_instrumentation.instrumentor import BaseInstrumentor
+
+from opentelemetry.instrumentors.django.middleware import (
+    OpenTelemetryMiddleware
+)
+
+_logger = getLogger(__name__)
+
+
+class DjangoInstrumentor(BaseInstrumentor):
+    """A instrumentor for flask.Django
+
+    See `BaseInstrumentor`
+    """
+
+    def _instrument(self):
+        # Django Middleware is code that is executed before and/or after a
+        # request. Read about Django middleware here:
+        # https://docs.djangoproject.com/en/3.0/topics/http/middleware/
+
+        # This method must set this Django settings attribute:
+        # MIDDLEWARE: for Django version > 1.0
+        # MIDDLEWARE_CLASSES: for Django version <= 1.0
+
+        # Django settings.MIDDLEWARE is a list of strings, each one a Python
+        # path to a class or a function that acts as middleware.
+
+        self._middleware_setting = (
+            "MIDDLEWARE" if VERSION >= (1, 10, 0) else "MIDDLEWARE_CLASSES"
+        )
+
+        settings_middleware = getattr(
+            settings,
+            self._middleware_setting,
+            []
+        )
+        settings_middleware.append(
+            ".".join(
+                [
+                    OpenTelemetryMiddleware.__module__,
+                    OpenTelemetryMiddleware.__qualname__
+                ]
+            )
+        )
+        setattr(
+            settings,
+            self._middleware_setting,
+            settings_middleware
+        )
+
+    def _uninstrument(self):
+        pass
