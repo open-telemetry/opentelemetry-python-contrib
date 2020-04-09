29664ffea2a6c5759880aa6eea77acb36a47f2ad
Mauricio Vásquez <mauricio@kinvolk.io>
2020-04-09 11:23:46 -0500

    Remove specific names about opentelemetry-python

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -15,15 +15,5 @@ function cov {
 
 coverage erase
 
-cov opentelemetry-api
-cov opentelemetry-sdk
-cov ext/opentelemetry-ext-flask
-cov ext/opentelemetry-ext-http-requests
-cov ext/opentelemetry-ext-jaeger
-cov ext/opentelemetry-ext-opentracing-shim
-cov ext/opentelemetry-ext-wsgi
-cov ext/opentelemetry-ext-zipkin
-cov docs/examples/opentelemetry-example-app
-
 coverage report
 coverage xml
