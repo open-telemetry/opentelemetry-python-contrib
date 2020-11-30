5b22dfd1584895859788c2676bf426d82487739f
Max Nikitenko <moaddib666@gmail.com>
2020-11-30 18:36:57 +0200

    Update instrumentation/opentelemetry-instrumentation-urllib/src/opentelemetry/instrumentation/urllib/__init__.py
    Co-authored-by: Leighton Chen <lechen@microsoft.com>

--- instrumentation/opentelemetry-instrumentation-urllib/src/opentelemetry/instrumentation/urllib/__init__.py
+++ instrumentation/opentelemetry-instrumentation-urllib/src/opentelemetry/instrumentation/urllib/__init__.py
@@ -27,7 +27,7 @@ Usage
 
     # You can optionally pass a custom TracerProvider to
     URLLibInstrumentor().instrument()
-
+URLLibInstrumentor().instrument()
     req = request.Request('https://postman-echo.com/post', method="POST")
     r = request.urlopen(req)
 
