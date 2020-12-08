da8e6d94a7b95da35aa97ff4d4e2b9af3ae215cd
Leighton <lechen@microsoft.com>
2020-12-08 10:48:09 -0500

    lint

--- instrumentation/opentelemetry-instrumentation-wsgi/tests/test_getter.py
+++ instrumentation/opentelemetry-instrumentation-wsgi/tests/test_getter.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import mock, TestCase
+from unittest import TestCase, mock
+
 from opentelemetry.instrumentation.wsgi import CarrierGetter
 
 
