f4897199c89c9ff5ee4097316db1187651aee19d
Leighton Chen <lechen@microsoft.com>
2021-02-03 20:46:01 -0800

    grpc

--- instrumentation/opentelemetry-instrumentation-django/src/opentelemetry/instrumentation/django/middleware.py
+++ instrumentation/opentelemetry-instrumentation-django/src/opentelemetry/instrumentation/django/middleware.py
@@ -87,21 +87,6 @@ class _DjangoMiddleware(MiddlewareMixin):
         except Resolver404:
             return "HTTP {}".format(request.method)
 
-    @staticmethod
-    def _get_metric_labels_from_attributes(attributes):
-        labels = {}
-        labels["http.method"] = attributes.get("http.method", "")
-        for attrs in _attributes_by_preference:
-            labels_from_attributes = {
-                attr: attributes.get(attr, None) for attr in attrs
-            }
-            if set(attrs).issubset(attributes.keys()):
-                labels.update(labels_from_attributes)
-                break
-        if attributes.get("http.flavor"):
-            labels["http.flavor"] = attributes.get("http.flavor")
-        return labels
-
     def process_request(self, request):
         # request.META is a dictionary containing all available HTTP headers
         # Read more about request.META here:
@@ -109,9 +94,6 @@ class _DjangoMiddleware(MiddlewareMixin):
 
         if self._excluded_urls.url_disabled(request.build_absolute_uri("?")):
             return
-
-        # pylint:disable=W0212
-        request._otel_start_time = time.time()
 
         environ = request.META
 
@@ -128,10 +110,6 @@ class _DjangoMiddleware(MiddlewareMixin):
         )
 
         attributes = collect_request_attributes(environ)
-        # pylint:disable=W0212
-        request._otel_labels = self._get_metric_labels_from_attributes(
-            attributes
-        )
 
         if span.is_recording():
             attributes = extract_attributes_from_object(
@@ -187,10 +165,6 @@ class _DjangoMiddleware(MiddlewareMixin):
                 "{} {}".format(response.status_code, response.reason_phrase),
                 response,
             )
-            # pylint:disable=W0212
-            request._otel_labels["http.status_code"] = str(
-                response.status_code
-            )
             request.META.pop(self._environ_span_key)
 
             exception = request.META.pop(self._environ_exception_key, None)
@@ -209,14 +183,3 @@ class _DjangoMiddleware(MiddlewareMixin):
         if self._environ_token in request.META.keys():
             detach(request.environ.get(self._environ_token))
             request.META.pop(self._environ_token)
-
-        try:
-            metric_recorder = getattr(settings, "OTEL_METRIC_RECORDER", None)
-            if metric_recorder is not None:
-                # pylint:disable=W0212
-                metric_recorder.record_server_duration_range(
-                    request._otel_start_time, time.time(), request._otel_labels
-                )
-        except Exception as ex:  # pylint: disable=W0703
-            _logger.warning("Error recording duration metrics: %s", ex)
-        return response
--- instrumentation/opentelemetry-instrumentation-grpc/src/opentelemetry/instrumentation/grpc/__init__.py
+++ instrumentation/opentelemetry-instrumentation-grpc/src/opentelemetry/instrumentation/grpc/__init__.py
@@ -33,10 +33,6 @@ Usage Client
         SimpleExportSpanProcessor,
     )
 
-    from opentelemetry import metrics
-    from opentelemetry.sdk.metrics import MeterProvider
-    from opentelemetry.sdk.metrics.export import ConsoleMetricsExporter
-
     try:
         from .gen import helloworld_pb2, helloworld_pb2_grpc
     except ImportError:
@@ -47,13 +43,7 @@ Usage Client
         SimpleExportSpanProcessor(ConsoleSpanExporter())
     )
 
-    # Set meter provider to opentelemetry-sdk's MeterProvider
-    metrics.set_meter_provider(MeterProvider())
-
-    # Optional - export GRPC specific metrics (latency, bytes in/out, errors) by passing an exporter
-    instrumentor = GrpcInstrumentorClient().instrument(
-        exporter = ConsoleMetricsExporter(),
-        interval = 10)
+    instrumentor = GrpcInstrumentorClient()
 
     def run():
         with grpc.insecure_channel("localhost:50051") as channel:
@@ -128,7 +118,7 @@ You can also add the instrumentor manually, rather than using
                          interceptors = [server_interceptor()])
 
 """
-from functools import partial
+from contextlib import contextmanager
 
 import grpc  # pylint:disable=import-self
 from wrapt import wrap_function_wrapper as _wrap
@@ -185,22 +175,12 @@ class GrpcInstrumentorClient(BaseInstrumentor):
 
     """
 
-    def _instrument(self, **kwargs):
-        exporter = kwargs.get("exporter", None)
-        interval = kwargs.get("interval", 30)
+    def _instrument(self, **kwargs):    
         if kwargs.get("channel_type") == "secure":
-            _wrap(
-                "grpc",
-                "secure_channel",
-                partial(self.wrapper_fn, exporter, interval),
-            )
+            _wrap("grpc", "secure_channel", self.wrapper_fn)
 
         else:
-            _wrap(
-                "grpc",
-                "insecure_channel",
-                partial(self.wrapper_fn, exporter, interval),
-            )
+            _wrap("grpc", "insecure_channel", self.wrapper_fn)
 
     def _uninstrument(self, **kwargs):
         if kwargs.get("channel_type") == "secure":
@@ -209,28 +189,17 @@ class GrpcInstrumentorClient(BaseInstrumentor):
         else:
             unwrap(grpc, "insecure_channel")
 
-    def wrapper_fn(
-        self, exporter, interval, original_func, instance, args, kwargs
-    ):
-        channel = original_func(*args, **kwargs)
-        tracer_provider = kwargs.get("tracer_provider")
-        return intercept_channel(
-            channel,
-            client_interceptor(
-                tracer_provider=tracer_provider,
-                exporter=exporter,
-                interval=interval,
-            ),
-        )
+    @contextmanager
+    def wrapper_fn(self, original_func, instance, args, kwargs):
+        with original_func(*args, **kwargs) as channel:
+            yield intercept_channel(channel, client_interceptor())
 
 
-def client_interceptor(tracer_provider=None, exporter=None, interval=30):
+def client_interceptor(tracer_provider=None):
     """Create a gRPC client channel interceptor.
 
     Args:
         tracer: The tracer to use to create client-side spans.
-        exporter: The exporter that will receive client metrics
-        interval: Time between every export call
 
     Returns:
         An invocation-side interceptor object.
@@ -239,7 +208,7 @@ def client_interceptor(tracer_provider=None, exporter=None, interval=30):
 
     tracer = trace.get_tracer(__name__, __version__, tracer_provider)
 
-    return _client.OpenTelemetryClientInterceptor(tracer, exporter, interval)
+    return _client.OpenTelemetryClientInterceptor(tracer)
 
 
 def server_interceptor(tracer_provider=None):
