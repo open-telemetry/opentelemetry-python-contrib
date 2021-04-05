ccc57a67cb56ed550125f88214c34a36c148324e
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-04-05 16:42:33 +0530

    Remove http.status_text from span attributes

--- instrumentation/opentelemetry-instrumentation-django/tests/test_middleware.py
+++ instrumentation/opentelemetry-instrumentation-django/tests/test_middleware.py
@@ -116,7 +116,6 @@ class TestMiddleware(TestBase, WsgiTestBase):
         )
         self.assertEqual(span.attributes["http.scheme"], "http")
         self.assertEqual(span.attributes["http.status_code"], 200)
-        self.assertEqual(span.attributes["http.status_text"], "OK")
 
     def test_traced_get(self):
         Client().get("/traced/")
@@ -138,7 +137,6 @@ class TestMiddleware(TestBase, WsgiTestBase):
         self.assertEqual(span.attributes["http.route"], "^traced/")
         self.assertEqual(span.attributes["http.scheme"], "http")
         self.assertEqual(span.attributes["http.status_code"], 200)
-        self.assertEqual(span.attributes["http.status_text"], "OK")
 
     def test_not_recording(self):
         mock_tracer = Mock()
@@ -173,7 +171,6 @@ class TestMiddleware(TestBase, WsgiTestBase):
         self.assertEqual(span.attributes["http.route"], "^traced/")
         self.assertEqual(span.attributes["http.scheme"], "http")
         self.assertEqual(span.attributes["http.status_code"], 200)
-        self.assertEqual(span.attributes["http.status_text"], "OK")
 
     def test_error(self):
         with self.assertRaises(ValueError):
