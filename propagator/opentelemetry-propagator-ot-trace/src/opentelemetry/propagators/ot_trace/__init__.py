0e09d95abce5b839d8f2a943d1ef34a8c06728f0
Mario Jonke <mario.jonke@dynatrace.com>
2021-05-04 16:42:56 +0200

    Make propagators conform to spec
    * do not modify / set an invalid span in the passed context in case
      a propagator did not manage to extract
    * in case no context is passed to propagator.extract assume the root
      context as default so that a new trace is started instead of continung
      the current active trace in case extraction fails
    * fix also ot-trace propagator which compared int with str trace/span ids
      when checking for validity in extract

--- propagator/opentelemetry-propagator-ot-trace/src/opentelemetry/propagators/ot_trace/__init__.py
+++ propagator/opentelemetry-propagator-ot-trace/src/opentelemetry/propagators/ot_trace/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from re import compile as re_compile
+import re
 from typing import Any, Iterable, Optional
 
 from opentelemetry.baggage import get_all, set_baggage
@@ -40,10 +40,10 @@ OT_SPAN_ID_HEADER = "ot-tracer-spanid"
 OT_SAMPLED_HEADER = "ot-tracer-sampled"
 OT_BAGGAGE_PREFIX = "ot-baggage-"
 
-_valid_header_name = re_compile(r"[\w_^`!#$%&'*+.|~]+")
-_valid_header_value = re_compile(r"[\t\x20-\x7e\x80-\xff]+")
-_valid_extract_traceid = re_compile(r"[0-9a-f]{1,32}")
-_valid_extract_spanid = re_compile(r"[0-9a-f]{1,16}")
+_valid_header_name = re.compile(r"[\w_^`!#$%&'*+.|~]+")
+_valid_header_value = re.compile(r"[\t\x20-\x7e\x80-\xff]+")
+_valid_extract_traceid = re.compile(r"[0-9a-f]{1,32}")
+_valid_extract_spanid = re.compile(r"[0-9a-f]{1,16}")
 
 
 class OTTracePropagator(TextMapPropagator):
@@ -55,13 +55,19 @@ class OTTracePropagator(TextMapPropagator):
         context: Optional[Context] = None,
         getter: Getter = default_getter,
     ) -> Context:
+        if context is None:
+            context = Context()
 
-        traceid = _extract_first_element(
-            getter.get(carrier, OT_TRACE_ID_HEADER), INVALID_TRACE_ID
+        traceid = _extract_identifier(
+            getter.get(carrier, OT_TRACE_ID_HEADER),
+            _valid_extract_traceid,
+            INVALID_TRACE_ID,
         )
 
-        spanid = _extract_first_element(
-            getter.get(carrier, OT_SPAN_ID_HEADER), INVALID_SPAN_ID
+        spanid = _extract_identifier(
+            getter.get(carrier, OT_SPAN_ID_HEADER),
+            _valid_extract_spanid,
+            INVALID_SPAN_ID,
         )
 
         sampled = _extract_first_element(
@@ -73,17 +79,12 @@ class OTTracePropagator(TextMapPropagator):
         else:
             traceflags = TraceFlags.DEFAULT
 
-        if (
-            traceid != INVALID_TRACE_ID
-            and _valid_extract_traceid.fullmatch(traceid) is not None
-            and spanid != INVALID_SPAN_ID
-            and _valid_extract_spanid.fullmatch(spanid) is not None
-        ):
+        if traceid != INVALID_TRACE_ID and spanid != INVALID_SPAN_ID:
             context = set_span_in_context(
                 NonRecordingSpan(
                     SpanContext(
-                        trace_id=int(traceid, 16),
-                        span_id=int(spanid, 16),
+                        trace_id=traceid,
+                        span_id=spanid,
                         is_remote=True,
                         trace_flags=TraceFlags(traceflags),
                     )
@@ -172,3 +173,16 @@ def _extract_first_element(
     if items is None:
         return default
     return next(iter(items), None)
+
+
+def _extract_identifier(
+    items: Iterable[CarrierT], validator: re.Pattern, default: int
+) -> int:
+    header = _extract_first_element(items)
+    if header is None or validator.fullmatch(header) is None:
+        return default
+
+    try:
+        return int(header, 16)
+    except ValueError:
+        return default
