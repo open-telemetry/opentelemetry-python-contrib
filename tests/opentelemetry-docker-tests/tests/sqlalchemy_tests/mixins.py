3d4087c29bb33025efa6fc8a1c0992055ba88885
Matthew Brown <me@matthewbrown.io>
2021-07-06 17:07:26 +0100

    account for add_all optimisations in sqlalchemy 1.4

--- tests/opentelemetry-docker-tests/tests/sqlalchemy_tests/mixins.py
+++ tests/opentelemetry-docker-tests/tests/sqlalchemy_tests/mixins.py
@@ -243,4 +243,4 @@ class SQLAlchemyTestMixin(TestBase):
                 close_all_sessions()
 
         spans = self.memory_exporter.get_finished_spans()
-        self.assertEqual(len(spans), 5)
+        self.assertEqual(len(spans), 5 if self.VENDOR not in ["postgresql"] else 3)
