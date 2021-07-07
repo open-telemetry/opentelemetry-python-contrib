0eabf8e7ec014266abd17c7e5d4002385c5a33c2
Matthew Brown <me@matthewbrown.io>
2021-07-07 14:09:12 +0100

    use pytest mark to skip tests. fix work directory for sqlalchemy tests

--- instrumentation/opentelemetry-instrumentation-sqlalchemy/tests/test_sqlalchemy.py
+++ instrumentation/opentelemetry-instrumentation-sqlalchemy/tests/test_sqlalchemy.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
-from typing import Coroutine
-from unittest import mock
+from unittest import IsolatedAsyncioTestCase, mock
 
+import pytest
 import sqlalchemy
 from sqlalchemy import create_engine
 
@@ -23,11 +22,7 @@ from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 from opentelemetry.test.test_base import TestBase
 
 
-def _call_async(coro: Coroutine):
-    return asyncio.get_event_loop().run_until_complete(coro)
-
-
-class TestSqlalchemyInstrumentation(TestBase):
+class TestSqlalchemyInstrumentation(TestBase, IsolatedAsyncioTestCase):
     def tearDown(self):
         super().tearDown()
         SQLAlchemyInstrumentor().uninstrument()
@@ -45,9 +40,11 @@ class TestSqlalchemyInstrumentation(TestBase):
         self.assertEqual(spans[0].name, "SELECT :memory:")
         self.assertEqual(spans[0].kind, trace.SpanKind.CLIENT)
 
-    def test_async_trace_integration(self):
-        if sqlalchemy.__version__.startswith("1.3"):
-            return
+    @pytest.mark.skipif(
+        sqlalchemy.__version__.startswith("1.3"),
+        reason="skipping async tests for 1.3",
+    )
+    async def test_async_trace_integration(self):
         from sqlalchemy.ext.asyncio import (  # pylint: disable-all
             create_async_engine,
         )
@@ -56,9 +53,8 @@ class TestSqlalchemyInstrumentation(TestBase):
         SQLAlchemyInstrumentor().instrument(
             engine=engine.sync_engine, tracer_provider=self.tracer_provider
         )
-        cnx = _call_async(engine.connect())
-        _call_async(cnx.execute(sqlalchemy.text("SELECT	1 + 1;"))).fetchall()
-        _call_async(cnx.close())
+        async with engine.connect() as cnx:
+            await cnx.execute(sqlalchemy.text("SELECT	1 + 1;"))
         spans = self.memory_exporter.get_finished_spans()
         self.assertEqual(len(spans), 1)
         self.assertEqual(spans[0].name, "SELECT :memory:")
@@ -95,18 +91,19 @@ class TestSqlalchemyInstrumentation(TestBase):
         self.assertEqual(spans[0].name, "SELECT :memory:")
         self.assertEqual(spans[0].kind, trace.SpanKind.CLIENT)
 
-    def test_create_async_engine_wrapper(self):
+    @pytest.mark.skipif(
+        sqlalchemy.__version__.startswith("1.3"),
+        reason="skipping async tests for 1.3",
+    )
+    async def test_create_async_engine_wrapper(self):
         SQLAlchemyInstrumentor().instrument()
-        if sqlalchemy.__version__.startswith("1.3"):
-            return
         from sqlalchemy.ext.asyncio import (  # pylint: disable-all
             create_async_engine,
         )
 
         engine = create_async_engine("sqlite+aiosqlite:///:memory:")
-        cnx = _call_async(engine.connect())
-        _call_async(cnx.execute(sqlalchemy.text("SELECT	1 + 1;"))).fetchall()
-        _call_async(cnx.close())
+        async with engine.connect() as cnx:
+            await cnx.execute(sqlalchemy.text("SELECT	1 + 1;"))
         spans = self.memory_exporter.get_finished_spans()
         self.assertEqual(len(spans), 1)
         self.assertEqual(spans[0].name, "SELECT :memory:")
