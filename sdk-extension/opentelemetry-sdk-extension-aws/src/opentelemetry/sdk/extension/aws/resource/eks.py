2b0457828649ba2a6123f4b979a9cc1c21f9c45d
(Eliseo) Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2021-07-14 11:41:39 -0700

    Small updates to EKS resource detector

--- sdk-extension/opentelemetry-sdk-extension-aws/src/opentelemetry/sdk/extension/aws/resource/eks.py
+++ sdk-extension/opentelemetry-sdk-extension-aws/src/opentelemetry/sdk/extension/aws/resource/eks.py
@@ -32,15 +32,17 @@ _CONTAINER_ID_LENGTH = 64
 _GET_METHOD = "GET"
 
 
-def _aws_http_request(method, path, cred_value, cert):
+def _aws_http_request(method, path, cred_value, cert_data):
     response = urlopen(
         Request(
-            "http://kubernetes.default.svc" + path, headers={"Authorization": cred_value}, method=method
+            "https://kubernetes.default.svc" + path,
+            headers={"Authorization": cred_value},
+            method=method,
         ),
         timeout=2000,
-        context=ssl.create_default_context(cafile=cert)
+        context=ssl.create_default_context(cadata=cert_data),
     )
-    return response.read().decode('utf-8')
+    return response.read().decode("utf-8")
 
 
 def _get_k8s_cred_value():
@@ -54,44 +56,50 @@ def _get_k8s_cred_value():
         return ""
 
 
-def _is_Eks(cert, cred_value):
+def _is_Eks(cred_value, cert_data):
     return _aws_http_request(
         _GET_METHOD,
         "/api/v1/namespaces/kube-system/configmaps/aws-auth",
         cred_value,
-        cert,
+        cert_data,
     )
 
 
-def _get_cluster_info(cert, cred_value):
+def _get_cluster_info(cred_value, cert_data):
     return _aws_http_request(
         _GET_METHOD,
         "/api/v1/namespaces/amazon-cloudwatch/configmaps/cluster-info",
         cred_value,
-        cert,
+        cert_data,
     )
 
 
 class AwsEksResourceDetector(ResourceDetector):
     def detect(self) -> "Resource":
         try:
-
-            cred_value = _get_k8s_cred_value()
             with open(
                 "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
             ) as f:
-                k8_cert = f.read()
-                if not _is_Eks(k8_cert, cred_value):
+                k8_cert_data = f.read()
+                if not _is_Eks(_get_k8s_cred_value(), k8_cert_data):
                     return Resource.get_empty()
 
+            container_id = ""
             with open("proc/self/cgroup", encoding="utf8") as f:
                 for line in f.readlines():
                     if len(line) > _CONTAINER_ID_LENGTH:
                         container_id = line[-_CONTAINER_ID_LENGTH:]
 
-            cluster_info = json.load(_get_cluster_info(k8_cert, cred_value))
+            cluster_info = json.loads(
+                _get_cluster_info(_get_k8s_cred_value(), k8_cert_data)
+            )
+            cluster_name = ""
+            try:
+                cluster_name = cluster_info["data"]["cluster.name"]
+            except Exception as e:
+                logger.warn(f"Cannot get cluster name on EKS: {e}")
 
-            if not container_id or not cluster_info:
+            if not container_id or not cluster_name:
                 return Resource.get_empty()
 
             # NOTE: (NathanielRN) Should ResourceDetectors use Resource.create() to pull in the environment variable?
@@ -100,9 +108,7 @@ class AwsEksResourceDetector(ResourceDetector):
                 {
                     ResourceAttributes.CLOUD_PROVIDER: CloudProviderValues.AWS.value,
                     ResourceAttributes.CLOUD_PLATFORM: CloudPlatformValues.AWS_EKS.value,
-                    ResourceAttributes.K8S_CLUSTER_NAME: cluster_info["data"][
-                        "cluster.name"
-                    ],
+                    ResourceAttributes.K8S_CLUSTER_NAME: cluster_name,
                     ResourceAttributes.CONTAINER_ID: container_id,
                 }
             )
