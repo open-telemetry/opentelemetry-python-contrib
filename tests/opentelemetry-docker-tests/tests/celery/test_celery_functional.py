522b3e7b3a4af859c8c687f0c841f9aeee0156d5
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-08-30 17:26:39 +0200

    Rerun a flaky test
    Fixes #638

--- tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
+++ tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
@@ -14,7 +14,7 @@
 
 
 import celery
-import pytest
+from pytest import mark
 from celery.exceptions import Retry
 
 import opentelemetry.instrumentation.celery
@@ -33,7 +33,7 @@ class MyException(Exception):
     pass
 
 
-@pytest.mark.skip(reason="inconsistent test results")
+@mark.skip(reason="inconsistent test results")
 def test_instrumentation_info(celery_app, memory_exporter):
     @celery_app.task
     def fn_task():
@@ -143,7 +143,7 @@ def test_fn_task_apply_bind(celery_app, memory_exporter):
     assert span.attributes.get("celery.state") == "SUCCESS"
 
 
-@pytest.mark.skip(reason="inconsistent test results")
+@mark.skip(reason="inconsistent test results")
 def test_fn_task_apply_async(celery_app, memory_exporter):
     @celery_app.task
     def fn_task_parameters(user, force_logout=False):
@@ -190,7 +190,7 @@ def test_fn_task_apply_async(celery_app, memory_exporter):
     )
 
 
-@pytest.mark.skip(reason="inconsistent test results")
+@mark.skip(reason="inconsistent test results")
 def test_concurrent_delays(celery_app, memory_exporter):
     @celery_app.task
     def fn_task():
@@ -206,7 +206,7 @@ def test_concurrent_delays(celery_app, memory_exporter):
     assert len(spans) == 200
 
 
-@pytest.mark.skip(reason="inconsistent test results")
+@mark.skip(reason="inconsistent test results")
 def test_fn_task_delay(celery_app, memory_exporter):
     @celery_app.task
     def fn_task_parameters(user, force_logout=False):
@@ -476,7 +476,7 @@ def test_shared_task(celery_app, memory_exporter):
     )
 
 
-@pytest.mark.skip(reason="inconsistent test results")
+@mark.skip(reason="inconsistent test results")
 def test_apply_async_previous_style_tasks(
     celery_app, celery_worker, memory_exporter
 ):
@@ -558,6 +558,8 @@ def test_apply_async_previous_style_tasks(
     ) == async_run_span.attributes.get(SpanAttributes.MESSAGING_MESSAGE_ID)
 
 
+# FIXME find a permanent solution for the flakiness of this test
+@mark.flaky
 def test_custom_tracer_provider(celery_app, memory_exporter):
     @celery_app.task
     def fn_task():
