f9ee2bb0de31c256600a39bdad8a2ece99efc7bd
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-09-07 11:58:43 +0200

    Try with a sleep of 1 second
    Fixes #653

--- tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
+++ tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
@@ -16,6 +16,7 @@
 import celery
 from celery.exceptions import Retry
 from pytest import mark
+from time import sleep
 
 import opentelemetry.instrumentation.celery
 from opentelemetry import trace as trace_api
@@ -33,13 +34,13 @@ class MyException(Exception):
     pass
 
 
-@mark.skip(reason="inconsistent test results")
 def test_instrumentation_info(celery_app, memory_exporter):
     @celery_app.task
     def fn_task():
         return 42
 
     result = fn_task.apply_async()
+    sleep(1)
     assert result.get(timeout=ASYNC_GET_TIMEOUT) == 42
 
     spans = memory_exporter.get_finished_spans()
