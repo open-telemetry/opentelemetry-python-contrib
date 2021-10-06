b5ed2f240c86062daecf43e052d99c2785b4443f
Ben Campbell <bcampbell@rechargeapps.com>
2021-10-06 14:51:06 -0700

    Fixing lint

--- instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
+++ instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
@@ -52,7 +52,11 @@ from opentelemetry.propagate import inject
 from opentelemetry.semconv.trace import SpanAttributes
 from opentelemetry.trace import SpanKind, get_tracer
 from opentelemetry.trace.status import Status
-from opentelemetry.util.http import remove_url_credentials, get_excluded_urls, parse_excluded_urls
+from opentelemetry.util.http import (
+    remove_url_credentials,
+    get_excluded_urls,
+    parse_excluded_urls,
+)
 from opentelemetry.util.http.httplib import set_ip_on_next_http_connection
 
 # A key to a context variable to avoid creating duplicate spans when instrumenting
