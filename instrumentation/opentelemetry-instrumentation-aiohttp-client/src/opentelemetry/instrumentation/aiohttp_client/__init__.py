5d4c78aa380618d9b1b560177697b49d90314b75
Leighton Chen <lechen@microsoft.com>
2021-10-26 09:55:44 -0700

    Update __init__.py

--- instrumentation/opentelemetry-instrumentation-aiohttp-client/src/opentelemetry/instrumentation/aiohttp_client/__init__.py
+++ instrumentation/opentelemetry-instrumentation-aiohttp-client/src/opentelemetry/instrumentation/aiohttp_client/__init__.py
@@ -238,11 +238,7 @@ def create_trace_config(
 
         if trace_config_ctx.span.is_recording():
             trace_config_ctx.span.set_status(
-                Status(
-                    http_status_to_status_code(
-                        int(params.response.status), server_span=True
-                    )
-                )
+                Status(http_status_to_status_code(int(params.response.status)))
             )
             trace_config_ctx.span.set_attribute(
                 SpanAttributes.HTTP_STATUS_CODE, params.response.status
