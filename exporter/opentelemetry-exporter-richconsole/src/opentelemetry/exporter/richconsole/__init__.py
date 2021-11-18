b982401a2f37c8e69d06718a2dcca60b6e4635b3
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-11-18 19:00:20 +1100

    Add a test

--- exporter/opentelemetry-exporter-richconsole/src/opentelemetry/exporter/richconsole/__init__.py
+++ exporter/opentelemetry-exporter-richconsole/src/opentelemetry/exporter/richconsole/__init__.py
@@ -36,13 +36,13 @@ all spans will be printed in a list.
 
     from opentelemetry import trace
     from opentelemetry.sdk.trace.export import BatchSpanProcessor
-    from opentelemetry.exporter.richconsole import RichConsoleExporter
+    from opentelemetry.exporter.richconsole import RichConsoleSpanExporter
     from opentelemetry.sdk.trace import TracerProvider
 
     trace.set_tracer_provider(TracerProvider())
     tracer = trace.get_tracer(__name__)
 
-    tracer.add_span_processor(BatchSpanProcessor(RichConsoleExporter()))
+    tracer.add_span_processor(BatchSpanProcessor(RichConsoleSpanExporter()))
 
 
 API
