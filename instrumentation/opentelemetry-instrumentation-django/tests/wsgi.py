08d8fce1d18bea923837e24f9b94bfeef4f11cad
Sanket Mehta <sanketmehta28@gmail.com>
2021-12-15 23:02:10 +0530

    Revert "Updating personal fork from public repo"

--- instrumentation/opentelemetry-instrumentation-django/tests/wsgi.py
+++
@@ -1,5 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
-
-application = get_wsgi_application()
