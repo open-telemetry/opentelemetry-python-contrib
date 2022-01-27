15dee7d5ac0d707bbd54100a00a0efb5da98dcf2
Alex Boten <aboten@lightstep.com>
2022-01-27 11:39:18 -0800

    update

--- instrumentation/opentelemetry-instrumentation-grpc/tests/test_client_interceptor.py
+++ instrumentation/opentelemetry-instrumentation-grpc/tests/test_client_interceptor.py
@@ -277,9 +277,7 @@ class TestClientProto(TestBase):
         previous_propagator = get_global_textmap()
         try:
             set_global_textmap(MockTextMapPropagator())
-            interceptor = OpenTelemetryClientInterceptor(
-                trace._NoOpTracer()
-            )
+            interceptor = OpenTelemetryClientInterceptor(trace.NoOpTracer())
 
             carrier = tuple()
 
