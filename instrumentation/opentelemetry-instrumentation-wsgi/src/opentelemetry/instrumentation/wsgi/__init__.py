49ed7273c0fdd86f6c2b5dfa707991948c76f784
Alex Boten <aboten@lightstep.com>
2022-03-10 09:14:55 -0800

    use is_recording

--- instrumentation/opentelemetry-instrumentation-wsgi/src/opentelemetry/instrumentation/wsgi/__init__.py
+++ instrumentation/opentelemetry-instrumentation-wsgi/src/opentelemetry/instrumentation/wsgi/__init__.py
@@ -314,10 +314,7 @@ class OpenTelemetryMiddleware:
         @functools.wraps(start_response)
         def _start_response(status, response_headers, *args, **kwargs):
             add_response_attributes(span, status, response_headers)
-            if (
-                not isinstance(span, NonRecordingSpan)
-                and span.kind == trace.SpanKind.SERVER
-            ):
+            if span.is_recording() and span.kind == trace.SpanKind.SERVER:
                 add_custom_response_headers(span, response_headers)
             if response_hook:
                 response_hook(status, response_headers)
@@ -340,10 +337,7 @@ class OpenTelemetryMiddleware:
             context_getter=wsgi_getter,
             attributes=collect_request_attributes(environ),
         )
-        if (
-            not isinstance(span, NonRecordingSpan)
-            and span.kind == trace.SpanKind.SERVER
-        ):
+        if span.is_recording() and span.kind == trace.SpanKind.SERVER:
             add_custom_request_headers(span, environ)
 
         if self.request_hook:
