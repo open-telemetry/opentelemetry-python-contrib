a1773d89ef487b82a20945a679f3f6c4dbddffc6
Diego Hurtado <ocelotl@users.noreply.github.com>
2022-03-21 17:39:30 -0600

    Mark benchmark tests as flaky
    Fixes #1016

--- sdk-extension/opentelemetry-sdk-extension-aws/tests/performance/benchmarks/trace/test_benchmark_aws_xray_ids_generator.py
+++ sdk-extension/opentelemetry-sdk-extension-aws/tests/performance/benchmarks/trace/test_benchmark_aws_xray_ids_generator.py
@@ -12,14 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from flaky import flaky
+
 from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
 
 id_generator = AwsXRayIdGenerator()
 
 
+@flaky(max_runs=3, min_passes=1)
 def test_generate_xray_trace_id(benchmark):
     benchmark(id_generator.generate_trace_id)
 
 
+@flaky(max_runs=3, min_passes=1)
 def test_generate_xray_span_id(benchmark):
     benchmark(id_generator.generate_span_id)
