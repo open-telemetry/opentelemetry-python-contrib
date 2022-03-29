464009b9f168dab5fd8f67cb73185083507ca58d
Ken Robbins <ken@surveymonkey.com>
2022-03-29 19:19:17 +0000

    fix lint again

--- instrumentation/opentelemetry-instrumentation-pyramid/tests/test_automatic.py
+++ instrumentation/opentelemetry-instrumentation-pyramid/tests/test_automatic.py
@@ -81,7 +81,9 @@ class TestAutomatic(InstrumentationTest, TestBase, WsgiTestBase):
 
     def test_registry_name_is_this_module(self):
         config = Configurator()
-        self.assertEqual(config.registry.__name__, __name__.rsplit(".", maxsplit=1)[0])
+        self.assertEqual(
+            config.registry.__name__, __name__.rsplit(".", maxsplit=1)[0]
+        )
 
 
 class TestWrappedWithOtherFramework(
