a0f6eeaf2c6a61277cb82342d4774015b3541841
dor <dor@oxeye.io>
2022-05-31 15:34:45 +0300

    fix tests

--- instrumentation/opentelemetry-instrumentation-confluent-kafka/tests/test_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-confluent-kafka/tests/test_instrumentation.py
@@ -14,6 +14,7 @@
 from unittest import TestCase
 
 from confluent_kafka import Consumer, Producer
+
 from opentelemetry.instrumentation.confluent_kafka import (
     ConfluentKafkaInstrumentor,
     ProxiedConsumer,
