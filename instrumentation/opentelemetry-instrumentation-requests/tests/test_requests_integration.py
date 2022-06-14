5c8cacbee07cd1e8e383463c2f25bd7982f962df
Ashutosh Goel <ashutgoe@cisco.com>
2022-06-14 16:34:22 +0530

    Fixing lint errors

--- instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
+++ instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
@@ -26,8 +26,6 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.utils import _SUPPRESS_INSTRUMENTATION_KEY
 from opentelemetry.propagate import get_global_textmap, set_global_textmap
 from opentelemetry.sdk import resources
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import InMemoryMetricReader
 from opentelemetry.semconv.trace import SpanAttributes
 from opentelemetry.test.mock_textmap import MockTextMapPropagator
 from opentelemetry.test.test_base import TestBase
