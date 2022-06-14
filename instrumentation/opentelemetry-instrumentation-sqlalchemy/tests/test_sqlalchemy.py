b48e4cd79d8e005427aacc8fa322d0326010a239
hemma <johan.bothin@gmail.com>
2022-06-14 22:34:32 +0200

    fix test

--- instrumentation/opentelemetry-instrumentation-sqlalchemy/tests/test_sqlalchemy.py
+++ instrumentation/opentelemetry-instrumentation-sqlalchemy/tests/test_sqlalchemy.py
@@ -55,6 +55,7 @@ class TestSqlalchemyInstrumentation(TestBase):
         engine_2 = create_engine("sqlite:///:memory:")
 
         SQLAlchemyInstrumentor().instrument(
+            engines=[engine_1, engine_2],
             tracer_provider=self.tracer_provider,
         )
 
