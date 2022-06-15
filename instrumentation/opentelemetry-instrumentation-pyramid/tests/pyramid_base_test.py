479324bd0bac098a23e4ddf82e34dff3e546eb67
Nina Stawski <nstawski@splunk.com>
2022-06-15 12:54:18 -0700

    pyramid: Only categorize 400s and 500s exceptions as errors

--- instrumentation/opentelemetry-instrumentation-pyramid/tests/pyramid_base_test.py
+++ instrumentation/opentelemetry-instrumentation-pyramid/tests/pyramid_base_test.py
@@ -24,6 +24,8 @@ class InstrumentationTest:
         helloid = int(request.matchdict["helloid"])
         if helloid == 500:
             raise exc.HTTPInternalServerError()
+        if helloid == 302:
+            raise exc.HTTPFound()
         if helloid == 900:
             raise NotImplementedError()
         return Response("Hello: " + str(helloid))
