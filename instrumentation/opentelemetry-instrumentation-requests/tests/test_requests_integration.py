b27340973c0c3410f0f451347941c43941bd22bb
Ashutosh Goel <39601429+ashu658@users.noreply.github.com>
2022-06-23 00:17:16 +0530

    Update instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
    Co-authored-by: Diego Hurtado <ocelotl@users.noreply.github.com>

--- instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
+++ instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
@@ -527,7 +527,6 @@ class TestRequestsIntergrationMetric(TestBase):
             for scope_metrics in resource_metrics.scope_metrics:
                 for metric in scope_metrics.metrics:
                     for data_point in metric.data.data_points:
-                        print(data_point.attributes)
                         self.assertDictEqual(
                             expected_attributes, dict(data_point.attributes)
                         )
