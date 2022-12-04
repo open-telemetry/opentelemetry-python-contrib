a279336c2a43f140f60925bf1ff5a2d2c07fd91b
Nimrod Shlagman <nemos@cisco.com>
2022-12-04 13:13:10 +0200

    fix aiopg version

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -25,7 +25,7 @@ libraries = {
         "instrumentation": "opentelemetry-instrumentation-aiohttp-client==0.36b0.dev",
     },
     "aiopg": {
-        "library": "aiopg >= 0.13.0, < 1.3.0",
+        "library": "aiopg >= 0.13.0, <= 1.4.0",
         "instrumentation": "opentelemetry-instrumentation-aiopg==0.36b0.dev",
     },
     "asgiref": {
