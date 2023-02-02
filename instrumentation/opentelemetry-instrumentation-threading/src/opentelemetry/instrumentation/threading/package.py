4d5625f2f7a14da9ee87efbca7394bdee008409e
pridhi-arora <110390842+pridhi-arora@users.noreply.github.com>
2023-02-02 19:14:22 +0530

    Update instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/package.py
    Co-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>

--- instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/package.py
+++ instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/package.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#To-do : what is the actual version of threading?
 _instruments = ()
 
 _supports_metrics = True
