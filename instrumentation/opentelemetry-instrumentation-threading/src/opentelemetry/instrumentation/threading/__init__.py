5e6bd840efd7e25bad34a5495aed0e2a00cef5d0
Pridhi Arora <pridhiarora17@gmail.com>
2023-02-16 22:33:34 +0530

    code cleanup

--- instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/__init__.py
+++ instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/__init__.py
@@ -18,8 +18,6 @@ import threading # pylint: disable=import-self
 from os import environ
 from typing import Collection
 from opentelemetry import context
-from wrapt import wrap_function_wrapper as _wrap
-from opentelemetry.instrumentation.utils import unwrap
 
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 
@@ -28,56 +26,10 @@ from opentelemetry.instrumentation.threading.version import __version__
 
 from opentelemetry import trace
 from opentelemetry.trace import (
-    INVALID_SPAN,
-    INVALID_SPAN_CONTEXT,
     get_current_span,
     get_tracer_provider,
-    get_tracer,
-    SpanKind
+    get_tracer
 )
-
-ATTRIBUTE_THREAD_NAME = "currentthread.name"
-DEFAULT_THREAD_NAME = "thread"
-ATTRIBUTE_TARGET_NAME = "currenttarget.name"
-DEFAULT_TARGET_NAME = "None"
-
-def _with_tracer_wrapper(func):
-    """Helper for providing tracer for wrapper functions."""
-
-    def _with_tracer(tracer):
-        def wrapper(wrapped, instance, args, kwargs):
-            return func(tracer, wrapped, instance, args, kwargs)
-
-        return wrapper
-
-    return _with_tracer
-
-def _wrap_target(ctx, target_func, tracer):
-    """Helper for providing tracer for wrapper functions."""
-    context.attach(ctx)
-    with tracer.start_as_current_span(
-        "threading.Thread.target",
-        kind=SpanKind.INTERNAL,
-    ) as span:
-        if span.is_recording():
-            span.set_attribute(ATTRIBUTE_TARGET_NAME, target_func.__name__)
-    return target_func
-
-@_with_tracer_wrapper
-def _wrap_thread(tracer, wrapped, instance, args, kwargs):
-    """Wrap `Threading.thread`"""
-
-    target_func = kwargs.get("target")
-    
-    with tracer.start_as_current_span(
-        "threading.Thread",
-        kind=SpanKind.INTERNAL,
-    ) as span:
-        if span.is_recording():
-            ctx = context.get_current()
-            kwargs["target"] = _wrap_target(ctx, target_func, tracer)
-            span.set_attribute(ATTRIBUTE_THREAD_NAME, wrapped.__name__)
-        return wrapped(*args, **kwargs)
 
 class _InstrumentedThread(threading.Thread):
     def __init__(self, *args, **kwargs):
