d491e944e5a6be0590e805a0cc2e06d63873332f
Adi Kochavi <akochavi@cisco.com>
2023-03-01 14:50:47 +0200

    wip

--- instrumentation/opentelemetry-instrumentation-celery/tests/test_metrics.py
+++ instrumentation/opentelemetry-instrumentation-celery/tests/test_metrics.py
@@ -1,14 +1,8 @@
 import threading
 import time
 from timeit import default_timer
-from typing import Optional, Union
 
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
-from opentelemetry.sdk.metrics._internal.point import Metric
-from opentelemetry.sdk.metrics.export import (
-    HistogramDataPoint,
-    NumberDataPoint,
-)
 from opentelemetry.test.test_base import TestBase
 
 from .celery_test_tasks import app, task_add
@@ -48,43 +42,6 @@ class TestMetrics(TestBase):
 
         return all_metrics
 
-    def assert_metric_expected(
-        self,
-        metric: Metric,
-        expected_value: Union[int, float],
-        expected_attributes: dict = None,
-        est_delta: Optional[float] = None,
-    ):
-        data_point = next(iter(metric.data.data_points))
-
-        if isinstance(data_point, HistogramDataPoint):
-            self.assertEqual(
-                data_point.count,
-                1,
-            )
-            if est_delta is None:
-                self.assertEqual(
-                    data_point.sum,
-                    expected_value,
-                )
-            else:
-                self.assertAlmostEqual(
-                    data_point.sum,
-                    expected_value,
-                    delta=est_delta,
-                )
-        elif isinstance(data_point, NumberDataPoint):
-            self.assertEqual(
-                data_point.value,
-                expected_value,
-            )
-
-        if expected_attributes:
-            self.assertDictEqual(
-                expected_attributes,
-                dict(data_point.attributes),
-            )
-
     def test_basic_metric(self):
         CeleryInstrumentor().instrument()
         start_time = default_timer()
@@ -99,12 +56,19 @@ class TestMetrics(TestBase):
         self.assertEqual(task_runtime.name, "flower.task.runtime.seconds")
         self.assert_metric_expected(
             task_runtime,
-            task_runtime_estimated,
-            {
-                "task": "tests.celery_test_tasks.task_add",
-                "worker": "celery@akochavi",
-            },
-            est_delta=200,
+            [
+                self.create_histogram_data_point(
+                    count=1,
+                    sum_data_point=task_runtime_estimated,
+                    max_data_point=task_runtime_estimated,
+                    min_data_point=task_runtime_estimated,
+                    attributes={
+                        "task": "tests.celery_test_tasks.task_add",
+                        "worker": "celery@akochavi",
+                    },
+                )
+            ],
+            est_value_delta=200,
         )
 
     def test_metric_uninstrument(self):
