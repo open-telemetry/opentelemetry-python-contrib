fe1bd893e4f19d7b05f51ca0cc85aaf6ad638f19
Matthew Grossman <matthewryangrossman@gmail.com>
2023-03-14 09:12:16 -0700

    weakref, not id()

--- instrumentation/opentelemetry-instrumentation-flask/src/opentelemetry/instrumentation/flask/__init__.py
+++ instrumentation/opentelemetry-instrumentation-flask/src/opentelemetry/instrumentation/flask/__init__.py
@@ -243,6 +243,7 @@ from packaging import version as package_version
 from time import time_ns
 from timeit import default_timer
 from typing import Collection
+import weakref
 
 import flask
 
@@ -265,18 +266,17 @@ _logger = getLogger(__name__)
 _ENVIRON_STARTTIME_KEY = "opentelemetry-flask.starttime_key"
 _ENVIRON_SPAN_KEY = "opentelemetry-flask.span_key"
 _ENVIRON_ACTIVATION_KEY = "opentelemetry-flask.activation_key"
-_ENVIRON_REQCTX_ID_KEY = "opentelemetry-flask.reqctx_id_key"
+_ENVIRON_REQCTX_REF_KEY = "opentelemetry-flask.reqctx_ref_key"
 _ENVIRON_TOKEN = "opentelemetry-flask.token"
 
 _excluded_urls_from_env = get_excluded_urls("FLASK")
 
 if package_version.parse(flask.__version__) >= package_version.parse("2.2.0"):
-    def _request_ctx_id() -> int:
-        # return id(flask.globals.request_ctx)
-        return id(flask.globals.request_ctx._get_current_object())
+    def _request_ctx_ref() -> weakref.ReferenceType:
+        return weakref.ref(flask.globals.request_ctx._get_current_object())
 else:
-    def _request_ctx_id() -> int:
-        return id(flask._request_ctx_stack.top)
+    def _request_ctx_ref() -> int:
+        return weakref.ref(flask._request_ctx_stack.top)
 
 def get_default_span_name():
     try:
@@ -406,7 +406,7 @@ def _wrapped_before_request(
         activation = trace.use_span(span, end_on_exit=True)
         activation.__enter__()  # pylint: disable=E1101
         flask_request_environ[_ENVIRON_ACTIVATION_KEY] = activation
-        flask_request_environ[_ENVIRON_REQCTX_ID_KEY] = _request_ctx_id()
+        flask_request_environ[_ENVIRON_REQCTX_REF_KEY] = _request_ctx_ref()
         flask_request_environ[_ENVIRON_SPAN_KEY] = span
         flask_request_environ[_ENVIRON_TOKEN] = token
 
@@ -447,9 +447,9 @@ def _wrapped_teardown_request(
 
         activation = flask.request.environ.get(_ENVIRON_ACTIVATION_KEY)
 
-        original_reqctx_id = flask.request.environ.get(_ENVIRON_REQCTX_ID_KEY)
-        current_reqctx_id = _request_ctx_id()
-        if not activation or original_reqctx_id != current_reqctx_id:
+        original_reqctx_ref = flask.request.environ.get(_ENVIRON_REQCTX_REF_KEY)
+        current_reqctx_ref = _request_ctx_ref()
+        if not activation or original_reqctx_ref != current_reqctx_ref:
             # This request didn't start a span, maybe because it was created in
             # a way that doesn't run `before_request`, like when it is created
             # with `app.test_request_context`.
