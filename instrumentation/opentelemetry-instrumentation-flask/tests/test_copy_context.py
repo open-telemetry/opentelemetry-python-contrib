7be29f3e12d307615eb06eeaac494988e2bfba80
Matthew Grossman <matthewryangrossman@gmail.com>
2023-04-07 14:24:33 -0700

    lint

--- instrumentation/opentelemetry-instrumentation-flask/tests/test_copy_context.py
+++ instrumentation/opentelemetry-instrumentation-flask/tests/test_copy_context.py
@@ -11,15 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import flask
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.test.wsgitestutil import WsgiTestBase
 from werkzeug.test import Client
 from werkzeug.wrappers import Response
 
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.test.wsgitestutil import WsgiTestBase
-
-# pylint: disable=import-error
 from .base_test import InstrumentationTest
 
 
@@ -41,12 +38,10 @@ class TestCopyContext(InstrumentationTest, WsgiTestBase):
         copied via `flask.copy_current_request_context`
         """
         self.app = flask.Flask(__name__)
-        self.app.route("/copy_context")(
-            self._copy_context_endpoint
-        )
+        self.app.route("/copy_context")(self._copy_context_endpoint)
         client = Client(self.app, Response)
-        resp = client.get("/copy_context", headers={'x-req': 'a-header'})
+        resp = client.get("/copy_context", headers={"x-req": "a-header"})
 
         self.assertEqual(200, resp.status_code)
-        self.assertEqual('/copy_context', resp.json['span_name'])
-        self.assertEqual('a-header', resp.json['request_header'])
+        self.assertEqual("/copy_context", resp.json["span_name"])
+        self.assertEqual("a-header", resp.json["request_header"])
