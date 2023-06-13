776f9d464361ae06ccf663e9b91ae7269882c800
Shalev Roda <65566801+shalevr@users.noreply.github.com>
2023-06-13 11:07:45 +0300

    Fix celery docker tests (#1841)

--- tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
+++ tests/opentelemetry-docker-tests/tests/celery/test_celery_functional.py
@@ -303,8 +303,8 @@ def test_fn_exception_expected(celery_app, memory_exporter):
 
     span = spans[0]
 
-    assert span.status.is_ok is True
-    assert span.status.status_code == StatusCode.UNSET
+    assert span.status.is_ok is False
+    assert span.status.status_code == StatusCode.ERROR
     assert span.name == "run/test_celery_functional.fn_exception"
     assert span.attributes.get("celery.action") == "run"
     assert span.attributes.get("celery.state") == "FAILURE"
@@ -443,8 +443,8 @@ def test_class_task_exception_excepted(celery_app, memory_exporter):
 
     span = spans[0]
 
-    assert span.status.is_ok is True
-    assert span.status.status_code == StatusCode.UNSET
+    assert span.status.is_ok is False
+    assert span.status.status_code == StatusCode.ERROR
     assert span.name == "run/test_celery_functional.BaseTask"
     assert span.attributes.get("celery.action") == "run"
     assert span.attributes.get("celery.state") == "FAILURE"
