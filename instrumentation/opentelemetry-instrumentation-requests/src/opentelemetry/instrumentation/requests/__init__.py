fa31c6f27a2cbb74c1821b90c1bedd37dbbd9cc4
Leighton Chen <lechen@microsoft.com>
2023-11-10 12:47:09 -0800

    sanitize

--- instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
+++ instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
@@ -81,6 +81,7 @@ from opentelemetry.util.http import (
     get_excluded_urls,
     parse_excluded_urls,
     remove_url_credentials,
+    sanitize_method,
 )
 from opentelemetry.util.http.httplib import set_ip_on_next_http_connection
 
@@ -254,7 +255,7 @@ def get_default_span_name(method):
     Returns:
         span name
     """
-    return method.strip()
+    return sanitize_method(method.strip())
 
 
 class RequestsInstrumentor(BaseInstrumentor):
