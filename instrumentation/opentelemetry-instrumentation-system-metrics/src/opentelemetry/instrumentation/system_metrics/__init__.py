f7c934ff76ef3dcc32a36617a24ff2d7d4179f5e
Rahul Kumar <Rahul.Kumar@fmr.com>
2023-11-16 20:07:22 +0530

    Changes requested by external reviewer Co-authored-by: Suryanarayana Peri <suryanarayana.peri@fmr.com>
    Signed-off-by: Rahul Kumar <Rahul.Kumar@fmr.com>

--- instrumentation/opentelemetry-instrumentation-system-metrics/src/opentelemetry/instrumentation/system_metrics/__init__.py
+++ instrumentation/opentelemetry-instrumentation-system-metrics/src/opentelemetry/instrumentation/system_metrics/__init__.py
@@ -92,7 +92,7 @@ from opentelemetry.instrumentation.system_metrics.version import __version__
 from opentelemetry.metrics import CallbackOptions, Observation, get_meter
 from opentelemetry.sdk.util import get_dict_as_key
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 _DEFAULT_CONFIG = {
@@ -355,16 +355,18 @@ class SystemMetricsInstrumentor(BaseInstrumentor):
                 unit="seconds",
             )
 
-        if "process.runtime.gc_count" in self._config and self._python_implementation != "pypy":
-            logger.warning(
-                    "The process.runtime.gc_count metric won't be collected because the interpreter is PyPy"
+        if "process.runtime.gc_count" in self._config:
+            if self._python_implementation == "pypy":
+                _logger.warning(
+                        "The process.runtime.gc_count metric won't be collected because the interpreter is PyPy"
                 )
-            self._meter.create_observable_counter(
-                name=f"process.runtime.{self._python_implementation}.gc_count",
-                callbacks=[self._get_runtime_gc_count],
-                description=f"Runtime {self._python_implementation} GC count",
-                unit="bytes",
-            )
+            else:
+                self._meter.create_observable_counter(
+                    name=f"process.runtime.{self._python_implementation}.gc_count",
+                    callbacks=[self._get_runtime_gc_count],
+                    description=f"Runtime {self._python_implementation} GC count",
+                    unit="bytes",
+                )
              
 
         if "process.runtime.thread_count" in self._config:
