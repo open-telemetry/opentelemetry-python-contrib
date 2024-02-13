9c7529414bdc082b57b7a2707ef8ec3fbed8806e
allen.k1m <allen.k1m@kakaocorp.com>
2024-02-13 10:55:00 +0900

    Removed from default_instrumentations

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -186,7 +186,6 @@ libraries = [
     },
 ]
 default_instrumentations = [
-    "opentelemetry-instrumentation-asyncio==0.44b0.dev",
     "opentelemetry-instrumentation-aws-lambda==0.44b0.dev",
     "opentelemetry-instrumentation-dbapi==0.44b0.dev",
     "opentelemetry-instrumentation-logging==0.44b0.dev",
