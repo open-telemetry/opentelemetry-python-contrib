d7ba7463d8a642665b3a58ebe260f0e47005b22e
Rodrigo <rodrigog.novas@mercadolibre.com>
2024-02-14 18:35:27 -0300

    feat: update to 132

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap.py
@@ -17,7 +17,7 @@ import logging
 import subprocess
 import sys
 
-import pkg_resources
+from importlib.metadata import distribution, PackageNotFoundError
 
 from opentelemetry.instrumentation.bootstrap_gen import (
     default_instrumentations,
@@ -84,17 +84,10 @@ def _pip_check():
 def _is_installed(req):
     if req in sys.modules:
         return True
-
+    
     try:
-        pkg_resources.get_distribution(req)
-    except pkg_resources.DistributionNotFound:
-        return False
-    except pkg_resources.VersionConflict as exc:
-        logger.warning(
-            "instrumentation for package %s is available but version %s is installed. Skipping.",
-            exc.req,
-            exc.dist.as_requirement(),  # pylint: disable=no-member
-        )
+        distribution(req)
+    except PackageNotFoundError:
         return False
     return True
 
