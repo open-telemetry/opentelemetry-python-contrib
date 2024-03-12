57bd9c092e974ebe3a29b7997f147e65f8d6808f
changemyminds <david7717318@gmail.com>
2024-03-12 23:42:34 +0800

    feat: support ThreadPool and update document

--- instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/__init__.py
+++ instrumentation/opentelemetry-instrumentation-threading/src/opentelemetry/instrumentation/threading/__init__.py
@@ -24,72 +24,103 @@ Usage
     ThreadingInstrumentor().instrument()
 
 This library provides instrumentation for the `threading` module to ensure that
-the OpenTelemetry context is propagated across threads.
+the OpenTelemetry context is propagated across threads. It is important to note
+that this instrumentation does not produce any telemetry data on its own. It
+merely ensures that the context is correctly propagated when threads are used.
 
-When instrumented, new threads created using `threading.Thread` or `threading.Timer`
-will have the current OpenTelemetry context attached, and this context will be
-re-activated in the thread's run method.
+
+When instrumented, new threads created using threading.Thread, threading.Timer, 
+or within futures.ThreadPoolExecutor will have the current OpenTelemetry 
+context attached, and this context will be re-activated in the thread's 
+run method or the executor's worker thread."
 """
 
 import threading
+from concurrent import futures
 from typing import Collection
 
 from wrapt import wrap_function_wrapper
 
-from opentelemetry import context, trace
+from opentelemetry import context
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.threading.package import _instruments
 from opentelemetry.instrumentation.utils import unwrap
 
 
 class ThreadingInstrumentor(BaseInstrumentor):
+    __WRAPPER_START_METHOD = "start"
+    __WRAPPER_RUN_METHOD = "run"
+    __WRAPPER_SUBMIT_METHOD = "submit"
+    __WRAPPER_KWARGS = "kwargs"
+    __WRAPPER_CONTEXT = "_otel_context"
+
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
 
     def _instrument(self, **kwargs):
         self._instrument_thread()
         self._instrument_timer()
+        self._instrument_thread_pool()
 
     def _uninstrument(self, **kwargs):
         self._uninstrument_thread()
         self._uninstrument_timer()
+        self._uninstrument_thread_pool()
 
     @staticmethod
     def _instrument_thread():
         wrap_function_wrapper(
             threading.Thread,
-            "start",
+            ThreadingInstrumentor.__WRAPPER_START_METHOD,
             ThreadingInstrumentor.__wrap_threading_start,
         )
         wrap_function_wrapper(
-            threading.Thread, "run", ThreadingInstrumentor.__wrap_threading_run
+            threading.Thread,
+            ThreadingInstrumentor.__WRAPPER_RUN_METHOD,
+            ThreadingInstrumentor.__wrap_threading_run,
         )
 
     @staticmethod
     def _instrument_timer():
         wrap_function_wrapper(
             threading.Timer,
-            "start",
+            ThreadingInstrumentor.__WRAPPER_START_METHOD,
             ThreadingInstrumentor.__wrap_threading_start,
         )
         wrap_function_wrapper(
-            threading.Timer, "run", ThreadingInstrumentor.__wrap_threading_run
+            threading.Timer,
+            ThreadingInstrumentor.__WRAPPER_RUN_METHOD,
+            ThreadingInstrumentor.__wrap_threading_run,
+        )
+
+    @staticmethod
+    def _instrument_thread_pool():
+        wrap_function_wrapper(
+            futures.ThreadPoolExecutor,
+            ThreadingInstrumentor.__WRAPPER_SUBMIT_METHOD,
+            ThreadingInstrumentor.__wrap_thread_pool_submit,
         )
 
     @staticmethod
     def _uninstrument_thread():
-        unwrap(threading.Thread, "start")
-        unwrap(threading.Thread, "run")
+        unwrap(threading.Thread, ThreadingInstrumentor.__WRAPPER_START_METHOD)
+        unwrap(threading.Thread, ThreadingInstrumentor.__WRAPPER_RUN_METHOD)
 
     @staticmethod
     def _uninstrument_timer():
-        unwrap(threading.Timer, "start")
-        unwrap(threading.Timer, "run")
+        unwrap(threading.Timer, ThreadingInstrumentor.__WRAPPER_START_METHOD)
+        unwrap(threading.Timer, ThreadingInstrumentor.__WRAPPER_RUN_METHOD)
+
+    @staticmethod
+    def _uninstrument_thread_pool():
+        unwrap(
+            futures.ThreadPoolExecutor,
+            ThreadingInstrumentor.__WRAPPER_SUBMIT_METHOD,
+        )
 
     @staticmethod
     def __wrap_threading_start(call_wrapped, instance, args, kwargs):
-        span = trace.get_current_span()
-        instance._otel_context = trace.set_span_in_context(span)
+        instance._otel_context = context.get_current()
         return call_wrapped(*args, **kwargs)
 
     @staticmethod
@@ -100,3 +131,30 @@ class ThreadingInstrumentor(BaseInstrumentor):
             return call_wrapped(*args, **kwargs)
         finally:
             context.detach(token)
+
+    @staticmethod
+    def __wrap_thread_pool_submit(call_wrapped, instance, args, kwargs):
+        # obtain the original function and wrapped kwargs
+        original_func = args[0]
+        wrapped_kwargs = {
+            ThreadingInstrumentor.__WRAPPER_KWARGS: kwargs,
+            ThreadingInstrumentor.__WRAPPER_CONTEXT: context.get_current(),
+        }
+
+        def wrapped_func(*func_args, **func_kwargs):
+            original_kwargs = func_kwargs.pop(
+                ThreadingInstrumentor.__WRAPPER_KWARGS
+            )
+            otel_context = func_kwargs.pop(
+                ThreadingInstrumentor.__WRAPPER_CONTEXT
+            )
+            token = None
+            try:
+                token = context.attach(otel_context)
+                return original_func(*func_args, **original_kwargs)
+            finally:
+                context.detach(token)
+
+        # replace the original function with the wrapped function
+        new_args = (wrapped_func,) + args[1:]
+        return call_wrapped(*new_args, **wrapped_kwargs)
