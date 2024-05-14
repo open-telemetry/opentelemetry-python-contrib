c195804fa224352cdd55f15e9c76567becd23ca4
Pablo Collins <pcollins@splunk.com>
2024-05-14 12:39:36 -0400

    start adding more tests

---
+++ util/oteltest/example_scripts/metrics_simple.py
@@ -0,0 +1,43 @@
+from typing import Mapping, Optional, Sequence
+
+from oteltest import OtelTest, Telemetry
+
+
+def print_time(iterations):
+    import time
+    from opentelemetry import trace, metrics
+    meter = metrics.get_meter('time-printer', '1.0')
+    print(f'got meter: {meter}')
+    counter = meter.create_counter('loop-counter', unit='1', description='my desc')
+    print(f'got counter: {counter}')
+    tracer = trace.get_tracer("my.tracer.name")
+    print(f'got tracer: {tracer}')
+    for i in range(iterations):
+        time.sleep(1)
+        counter.add(1)
+        with tracer.start_as_current_span('my-span') as span:
+            print(f'{i + 1}/{iterations} current time: {round(time.time())}')
+
+
+class MyTest(OtelTest):
+
+    def environment_variables(self) -> Mapping[str, str]:
+        return {}
+
+    def requirements(self) -> Sequence[str]:
+        return "opentelemetry-distro", "opentelemetry-exporter-otlp-proto-grpc"
+
+    def wrapper_command(self) -> str:
+        return "opentelemetry-instrument"
+
+    def on_start(self) -> Optional[float]:
+        print("started")
+        return None
+
+    def on_stop(self, telemetry: Telemetry, stdout: str, stderr: str, returncode: int) -> None:
+        print(f"stopped: {stdout}")
+        print(f"telemetry: {telemetry}")
+
+
+if __name__ == '__main__':
+    print_time(10)
--- util/oteltest/tests/test_oteltest.py
+++ util/oteltest/tests/test_oteltest.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
 
+from oteltest import Telemetry
 from oteltest.private import get_next_json_file, save_telemetry_json
 
 
@@ -31,3 +33,21 @@ def test_get_next_json_file(tmp_path):
 
     next_file = get_next_json_file(path_to_dir, module_name)
     assert "my_module_name.2.json" == next_file
+
+
+def test_telemetry():
+    t = Telemetry()
+    t.add_log({}, {}, 0)
+
+
+
+def json_to_telemetry(json_str: str) -> Telemetry:
+    return dict_to_telemetry(json.loads(json_str))
+
+
+def dict_to_telemetry(d) -> Telemetry:
+    return Telemetry(
+        log_requests=d["log_requests"],
+        metric_requests=d["metric_requests"],
+        trace_requests=d["trace_requests"],
+    )
