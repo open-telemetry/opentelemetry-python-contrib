c1785043b6845f11cc46298b67d8772635e66c3e
jeremydvoss <jerevoss@gmail.com>
2024-05-16 13:38:37 -0700

    Revert "Add deployment.environment to functions detector"
    This reverts commit 5411759711b8bc9976705deb416d5ffd8f65590f.

--- resource/opentelemetry-resource-detector-azure/tests/test_functions.py
+++ resource/opentelemetry-resource-detector-azure/tests/test_functions.py
@@ -21,7 +21,6 @@ from opentelemetry.resource.detector.azure.functions import (
 
 TEST_WEBSITE_SITE_NAME = "TEST_WEBSITE_SITE_NAME"
 TEST_REGION_NAME = "TEST_REGION_NAME"
-TEST_WEBSITE_SLOT_NAME = "TEST_WEBSITE_SLOT_NAME"
 TEST_WEBSITE_INSTANCE_ID = "TEST_WEBSITE_INSTANCE_ID"
 
 TEST_WEBSITE_RESOURCE_GROUP = "TEST_WEBSITE_RESOURCE_GROUP"
@@ -59,9 +58,6 @@ class TestAzureAppServiceResourceDetector(unittest.TestCase):
         )
 
         self.assertEqual(attributes["cloud.region"], TEST_REGION_NAME)
-        self.assertEqual(
-            attributes["deployment.environment"], TEST_WEBSITE_SLOT_NAME
-        )
         self.assertEqual(attributes["faas.instance"], TEST_WEBSITE_INSTANCE_ID)
         self.assertEqual(attributes["faas.max_memory"], 1024)
 
@@ -94,9 +90,6 @@ class TestAzureAppServiceResourceDetector(unittest.TestCase):
         )
 
         self.assertEqual(attributes["cloud.region"], TEST_REGION_NAME)
-        self.assertEqual(
-            attributes["deployment.environment"], TEST_WEBSITE_SLOT_NAME
-        )
         self.assertEqual(attributes["faas.instance"], TEST_WEBSITE_INSTANCE_ID)
         self.assertIsNone(attributes.get("faas.max_memory"))
 
