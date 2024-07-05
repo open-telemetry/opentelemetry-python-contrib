51f6f91e2fcab265c1f26836a1ce7d6d8821f75e
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-04 18:38:58 -0600

    try with tiwndos

--- .github/workflows/generate_matrix.py
+++ .github/workflows/generate_matrix.py
@@ -1,6 +1,7 @@
 from json import dumps
 
-matrix_values = [{"os": "ubuntu-latest"}]
+# matrix_values = [{"os": "ubuntu-latest"}]
+matrix_values = [{"os": ["ubuntu-latest", "windows-2019"]}]
 
 # print(f"name=matrix::{dumps(matrix_values)}")
 print(f"::set-output name=matrix::{dumps(matrix_values)}")
