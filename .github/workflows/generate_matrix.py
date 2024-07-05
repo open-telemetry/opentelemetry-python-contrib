3431f6ea902e5b3b27ac6f2e895915a8537cd478
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-05 16:38:28 -0600

    WUP

--- .github/workflows/generate_matrix.py
+++
@@ -1,8 +0,0 @@
-from json import dumps
-
-# matrix_values = [{"os": "ubuntu-latest"}]
-# matrix_values = [{"python_versions": ["py38", "py312"]}]
-python_versions = ["py38", "py312"]
-
-# print(f"name=matrix::{dumps(matrix_values)}")
-print(f"::set-output name=python_versions::{dumps(python_versions)}")
