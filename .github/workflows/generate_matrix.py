81319f87f6b787cf5a4d929428b6eb6922c504a7
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-04 18:27:33 -0600

    Try again with old commands

--- .github/workflows/generate_matrix.py
+++ .github/workflows/generate_matrix.py
@@ -2,4 +2,5 @@ from json import dumps
 
 matrix_values = [{"os": "ubuntu-latest"}]
 
-print(f"name=matrix::{dumps(matrix_values)}")
+# print(f"name=matrix::{dumps(matrix_values)}")
+print(f"::set-output name=matrix::{dumps(matrix_values)}")
