c4b8e0a13c9096684edf7353d6e42bd235502a7d
Michael Lorenzana <mlorenzana@gmail.com>
2024-07-29 10:30:10 -0400

    Removed issue with circular dependency on manual instrumentation.

--- instrumentation/opentelemetry-instrumentation-aws-lambda/src/opentelemetry/instrumentation/aws_lambda/__init__.py
+++ instrumentation/opentelemetry-instrumentation-aws-lambda/src/opentelemetry/instrumentation/aws_lambda/__init__.py
@@ -422,6 +422,8 @@ class AwsLambdaInstrumentor(BaseInstrumentor):
                     request.
         """
         lambda_handler = os.environ.get(ORIG_HANDLER, os.environ.get(_HANDLER))
+        if not lambda_handler:
+            return
         # pylint: disable=attribute-defined-outside-init
         (
             self._wrapped_module_name,
