265490fc78539b91ab7ae306ce924f10a4e14d9b
RJ Duffner <rjduffner@users.noreply.github.com>
2024-08-02 11:59:17 -0700

    kafka-python Instrument temporary fork, kafka-python-ng inside kafka-python's instrumentation (#2537)
    * Instrument temporary fork, kafka-python-ng inside kafka-python's instrumentation
    
    * Make kafkapython-ng run tests

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -105,7 +105,11 @@ libraries = [
         "instrumentation": "opentelemetry-instrumentation-jinja2==0.48b0.dev",
     },
     {
-        "library": "kafka-python >= 2.0",
+        "library": "kafka-python >= 2.0, < 3.0",
+        "instrumentation": "opentelemetry-instrumentation-kafka-python==0.48b0.dev",
+    },
+    {
+        "library": "kafka-python-ng >= 2.0, < 3.0",
         "instrumentation": "opentelemetry-instrumentation-kafka-python==0.48b0.dev",
     },
     {
