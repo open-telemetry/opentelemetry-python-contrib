3c103f950afeb3f0fa90a7544f991a86c0017ee5
Shi, Stone <jiadong.shi@fmr.com>
2024-09-26 18:47:09 +0800

    change code format
    Signed-off-by: Shi, Stone <jiadong.shi@fmr.com>

--- instrumentation/opentelemetry-instrumentation-system-metrics/src/opentelemetry/instrumentation/system_metrics/__init__.py
+++ instrumentation/opentelemetry-instrumentation-system-metrics/src/opentelemetry/instrumentation/system_metrics/__init__.py
@@ -397,13 +397,15 @@ class SystemMetricsInstrumentor(BaseInstrumentor):
                 unit="switches",
             )
 
-        if "process.open_file_descriptor.count" in self._config:
-            if sys.platform != "win32":
-                self._meter.create_observable_up_down_counter(
-                    name="process.open_file_descriptor.count",
-                    callbacks=[self._get_open_file_descriptors],
-                    description="Number of file descriptors in use by the process.",
-                )
+        if (
+            "process.open_file_descriptor.count" in self._config
+            and sys.platform != "win32"
+        ):
+            self._meter.create_observable_up_down_counter(
+                name="process.open_file_descriptor.count",
+                callbacks=[self._get_open_file_descriptors],
+                description="Number of file descriptors in use by the process.",
+            )
 
     def _uninstrument(self, **__):
         pass
