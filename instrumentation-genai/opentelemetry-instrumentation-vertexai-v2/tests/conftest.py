6ddf7e21d77c21d20cbab7d780b13c2b048671d6
Aaron Abbott <aaronabbott@google.com>
2024-12-05 16:22:54 +0000

    Working sanitized VCR for tests, but missing some assertions

--- instrumentation-genai/opentelemetry-instrumentation-vertexai-v2/tests/conftest.py
+++ instrumentation-genai/opentelemetry-instrumentation-vertexai-v2/tests/conftest.py
@@ -1,6 +1,11 @@
 """Unit tests configuration module."""
 
+from os import replace
+import re
+from typing import Any, Mapping, MutableMapping
+
 import pytest
+from google.auth.credentials import AnonymousCredentials
 
 from opentelemetry import trace
 from opentelemetry.instrumentation.vertexai_v2 import VertexAIInstrumentor
@@ -11,6 +16,11 @@ from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
 )
 
 pytest_plugins = []
+
+import vertexai
+from vcr import VCR
+from vcr.record_mode import RecordMode
+from vcr.request import Request
 
 
 @pytest.fixture(scope="session")
@@ -32,6 +42,55 @@ def clear_exporter(exporter):
     exporter.clear()
 
 
+@pytest.fixture(autouse=True)
+def vertexai_init(vcr: VCR) -> None:
+    # Unfortunately I couldn't find a nice way to globally reset the global_config for each
+    # test because different vertex submodules reference the global instance directly
+    # https://github.com/googleapis/python-aiplatform/blob/v1.74.0/google/cloud/aiplatform/initializer.py#L687
+    # so this config will leak if we don't call init() for each test.
+
+    # When not recording (in CI), don't do any auth. That prevents trying to read application
+    # default credentials from the filesystem or metadata server and oauth token exchange. This
+    # is not the interesting part of our instrumentation to test.
+    if vcr.record_mode is RecordMode.NONE:
+        vertexai.init(credentials=AnonymousCredentials())
+    else:
+        vertexai.init()
+
+
 @pytest.fixture(scope="module")
 def vcr_config():
-    return {"filter_headers": ["authorization"]}
+    filter_header_regexes = [
+        r"X-.*",
+        "Server",
+        "Date",
+        "Expires",
+        "Authorization",
+    ]
+
+    def filter_headers(headers: Mapping[str, str]) -> Mapping[str, str]:
+        return {
+            key: val
+            for key, val in headers.items()
+            if not any(
+                re.match(filter_re, key, re.IGNORECASE)
+                for filter_re in filter_header_regexes
+            )
+        }
+
+    def before_record_cb(request: Request):
+        request.headers = filter_headers(request.headers)
+        request.uri = re.sub(
+            r"/projects/[^/]+/", f"/projects/fake-project/", request.uri
+        )
+        return request
+
+    def before_response_cb(response: MutableMapping[str, Any]):
+        response["headers"] = filter_headers(response["headers"])
+        return response
+
+    return {
+        "before_record_request": before_record_cb,
+        "before_record_response": before_response_cb,
+        "ignore_hosts": ["oauth2.googleapis.com"],
+    }
---
+++ instrumentation-genai/opentelemetry-instrumentation-vertexai-v2/tests/test_gemini.py
@@ -0,0 +1,37 @@
+import pytest
+from vertexai.preview.generative_models import GenerativeModel, Part
+
+# from opentelemetry.semconv_ai import SpanAttributes
+
+
+@pytest.mark.vcr
+def test_vertexai_generate_content(exporter):
+    multimodal_model = GenerativeModel("gemini-pro-vision")
+    response = multimodal_model.generate_content(
+        [
+            Part.from_uri(
+                "gs://generativeai-downloads/images/scones.jpg",
+                mime_type="image/jpeg",
+            ),
+            "what is shown in this image?",
+        ]
+    )
+
+    spans = exporter.get_finished_spans()
+    assert [span.name for span in spans] == [
+        "text_completion gemini-pro-vision"
+    ]
+
+    vertexai_span = spans[0]
+    assert len(spans) == 1
+
+    assert vertexai_span.attributes == {
+        "gen_ai.system": "vertex_ai",
+        "gen_ai.operation.name": "text_completion",
+        "gen_ai.request.model": "gemini-pro-vision",
+        "gen_ai.response.model": "gemini-pro-vision",
+        "gen_ai.usage.output_tokens": 30,
+        "gen_ai.usage.input_tokens": 265,
+    }
+
+    # TODO: verify Events
