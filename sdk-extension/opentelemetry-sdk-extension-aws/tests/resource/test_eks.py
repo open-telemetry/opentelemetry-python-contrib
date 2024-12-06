ecbd817f834e45f7338313cfc27ab0d15ca54a24
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-12-06 14:54:16 +0100

    opentelemetry-sdk-extension-aws: make eks detector less chatty
    Don't print warnings if we are not running inside an eks instance so we can load the
    resource detector more generally and avoid warnings in stderr.

--- sdk-extension/opentelemetry-sdk-extension-aws/tests/resource/test_eks.py
+++ sdk-extension/opentelemetry-sdk-extension-aws/tests/resource/test_eks.py
@@ -43,6 +43,10 @@ class AwsEksResourceDetectorTest(unittest.TestCase):
         return_value=True,
     )
     @patch(
+        "opentelemetry.sdk.extension.aws.resource.eks._is_k8s",
+        return_value=True,
+    )
+    @patch(
         "opentelemetry.sdk.extension.aws.resource.eks._get_cluster_info",
         return_value=f"""{{
   "kind": "ConfigMap",
@@ -88,6 +92,7 @@ class AwsEksResourceDetectorTest(unittest.TestCase):
         self,
         mock_open_function,
         mock_get_cluster_info,
+        mock_is_k8s,
         mock_is_eks,
         mock_get_k8_cred_value,
     ):
@@ -104,8 +109,32 @@ class AwsEksResourceDetectorTest(unittest.TestCase):
         "opentelemetry.sdk.extension.aws.resource.eks._is_eks",
         return_value=False,
     )
+    @patch(
+        "opentelemetry.sdk.extension.aws.resource.eks._is_k8s",
+        return_value=True,
+    )
     def test_if_no_eks_env_var_and_should_raise(
-        self, mock_is_eks, mock_get_k8_cred_value
+        self, mock_is_k8s, mock_is_eks, mock_get_k8_cred_value
     ):
         with self.assertRaises(RuntimeError):
             AwsEksResourceDetector(raise_on_error=True).detect()
+
+    @patch(
+        "opentelemetry.sdk.extension.aws.resource.eks._get_k8s_cred_value",
+        return_value="MOCK_TOKEN",
+    )
+    @patch(
+        "opentelemetry.sdk.extension.aws.resource.eks._is_eks",
+        return_value=False,
+    )
+    @patch(
+        "opentelemetry.sdk.extension.aws.resource.eks._is_k8s",
+        return_value=False,
+    )
+    def test_if_no_eks_paths_should_not_raise(
+        self, mock_is_k8s, mock_is_eks, mock_get_k8_cred_value
+    ):
+        try:
+            AwsEksResourceDetector(raise_on_error=True).detect()
+        except RuntimeError:
+            self.fail("Should not raise")
