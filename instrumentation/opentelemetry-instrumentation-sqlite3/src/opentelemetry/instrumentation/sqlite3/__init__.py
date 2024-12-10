b42cc84036cd1afaa12778ea35a04749279d2eec
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-12-10 15:11:34 -0800

    Update sqlite3 instrumentor doc about explicit cursors

--- instrumentation/opentelemetry-instrumentation-sqlite3/src/opentelemetry/instrumentation/sqlite3/__init__.py
+++ instrumentation/opentelemetry-instrumentation-sqlite3/src/opentelemetry/instrumentation/sqlite3/__init__.py
@@ -14,7 +14,8 @@
 
 """
 SQLite instrumentation supporting `sqlite3`_, it can be enabled by
-using ``SQLite3Instrumentor``.
+using ``SQLite3Instrumentor``. At this time, cursor objects must
+be explicitly initialized as shown below to support tracing.
 
 .. _sqlite3: https://docs.python.org/3/library/sqlite3.html
 
