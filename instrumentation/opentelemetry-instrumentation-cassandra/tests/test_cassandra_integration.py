06d88dd47f948eac3163f6e1a94f204924293ddf
emdneto <9735060+emdneto@users.noreply.github.com>
2024-12-24 12:02:04 -0300

    cassandra: fix tests to make CI happy for py313
    Signed-off-by: emdneto <9735060+emdneto@users.noreply.github.com>

--- instrumentation/opentelemetry-instrumentation-cassandra/tests/test_cassandra_integration.py
+++ instrumentation/opentelemetry-instrumentation-cassandra/tests/test_cassandra_integration.py
@@ -42,7 +42,9 @@ class TestCassandraIntegration(TestBase):
         super().tearDown()
         with self.disable_logging():
             CassandraInstrumentor().uninstrument()
-
+    @property
+    def _mocked_session(self):
+        return cassandra.cluster.Session(cluster=mock.Mock(), hosts=[])
     def test_instrument_uninstrument(self):
         instrumentation = CassandraInstrumentor()
         instrumentation.instrument()
@@ -67,7 +69,7 @@ class TestCassandraIntegration(TestBase):
     ):
         mock_create_response_future.return_value = mock.Mock()
         mock_session_init.return_value = None
-        mock_connect.return_value = cassandra.cluster.Session()
+        mock_connect.return_value = self._mocked_session
 
         CassandraInstrumentor().instrument()
 
@@ -100,7 +102,7 @@ class TestCassandraIntegration(TestBase):
     ):
         mock_create_response_future.return_value = mock.Mock()
         mock_session_init.return_value = None
-        mock_connect.return_value = cassandra.cluster.Session()
+        mock_connect.return_value = self._mocked_session
 
         resource = resources.Resource.create({})
         result = self.create_tracer_provider(resource=resource)
@@ -124,7 +126,7 @@ class TestCassandraIntegration(TestBase):
     ):
         mock_create_response_future.return_value = mock.Mock()
         mock_session_init.return_value = None
-        mock_connect.return_value = cassandra.cluster.Session()
+        mock_connect.return_value = self._mocked_session
 
         tracer_provider = trace_api.NoOpTracerProvider()
         CassandraInstrumentor().instrument(tracer_provider=tracer_provider)
