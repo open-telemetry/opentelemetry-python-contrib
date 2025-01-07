ca3659ff5afea04faf8fd85c3870f716c7374f7b
Drew Robbins <drew@drewby.com>
2025-01-07 08:24:50 +0000

    Use semconv for metric

--- instrumentation-genai/opentelemetry-instrumentation-openai-v2/src/opentelemetry/instrumentation/openai_v2/meters.py
+++ instrumentation-genai/opentelemetry-instrumentation-openai-v2/src/opentelemetry/instrumentation/openai_v2/meters.py
@@ -1,12 +1,11 @@
+from opentelemetry.semconv._incubating.metrics import gen_ai_metrics
+
+
 class Meters:
     def __init__(self, meter):
-        self.operation_duration_histogram = meter.create_histogram(
-            name="gen_ai.client.operation.duration",
-            description="Duration of gen_ai client operations",
-            unit="seconds",
+        self.operation_duration_histogram = (
+            gen_ai_metrics.create_gen_ai_client_operation_duration(meter)
         )
-        self.token_usage_histogram = meter.create_histogram(
-            name="gen_ai.client.token.usage",
-            description="Token usage of gen_ai client operations",
-            unit="tokens",
+        self.token_usage_histogram = (
+            gen_ai_metrics.create_gen_ai_client_token_usage(meter)
         )
