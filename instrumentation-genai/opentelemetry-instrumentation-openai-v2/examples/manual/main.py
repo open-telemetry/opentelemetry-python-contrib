14f4470f8b2380a27d3e039aae2c7c975b908fb7
Liudmila Molkova <limolkova@microsoft.com>
2025-02-06 20:00:33 -0800

    leverage histogram bucket advice in 1.30

--- instrumentation-genai/opentelemetry-instrumentation-openai-v2/examples/manual/main.py
+++ instrumentation-genai/opentelemetry-instrumentation-openai-v2/examples/manual/main.py
@@ -8,23 +8,13 @@ from opentelemetry import _events, _logs, trace
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
     OTLPLogExporter,
 )
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
-    OTLPMetricExporter,
-)
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
 )
 from opentelemetry.instrumentation.openai_v2 import OpenAIInstrumentor
-from opentelemetry.metrics import set_meter_provider
 from opentelemetry.sdk._events import EventLoggerProvider
 from opentelemetry.sdk._logs import LoggerProvider
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk.metrics.view import (
-    ExplicitBucketHistogramAggregation,
-    View,
-)
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
@@ -33,66 +23,6 @@ trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(OTLPSpanExporter())
 )
-
-# configure metrics
-reader = PeriodicExportingMetricReader(
-    OTLPMetricExporter(), export_interval_millis=1000
-)
-
-# Configure metric views that allow to customize the bucket boundaries for histogram metrics.
-# This should not be necessary in the future - https://github.com/open-telemetry/opentelemetry-python-contrib/issues/3235.
-# The bucket boundaries ensure that the metrics are correctly aggregated and displayed in the backend.
-#
-# The bucket boundaries are defined as follows:
-# For `gen_ai.client.token.usage`: [1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864]
-# For `gen_ai.client.operation.duration`: [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12, 10.24, 20.48, 40.96, 81.92]
-#
-# If your application benefits from different bucket boundaries, you can update them as needed.
-views = [
-    View(
-        instrument_name="gen_ai.client.token.usage",
-        aggregation=ExplicitBucketHistogramAggregation(
-            [
-                1,
-                4,
-                16,
-                64,
-                256,
-                1024,
-                4096,
-                16384,
-                65536,
-                262144,
-                1048576,
-                4194304,
-                16777216,
-                67108864,
-            ]
-        ),
-    ),
-    View(
-        instrument_name="gen_ai.client.operation.duration",
-        aggregation=ExplicitBucketHistogramAggregation(
-            [
-                0.01,
-                0.02,
-                0.04,
-                0.08,
-                0.16,
-                0.32,
-                0.64,
-                1.28,
-                2.56,
-                5.12,
-                10.24,
-                20.48,
-                40.96,
-                81.92,
-            ]
-        ),
-    ),
-]
-set_meter_provider(MeterProvider(metric_readers=[reader], views=views))
 
 # configure logging and events
 _logs.set_logger_provider(LoggerProvider())
