3bd43abf630e048ad01b213c149c6fe5940eb2d0
Venkata Shreyas Kabekkodu <venkatashreyas.kabekkodu@fmr.com>
2025-02-26 14:42:25 +0530

    testing with botocoreinstrumentor
    Signed-off-by: Venkata Shreyas Kabekkodu <venkatashreyas.kabekkodu@fmr.com>

--- tests/opentelemetry-docker-tests/tests/boto/test_boto_functional.py
+++ tests/opentelemetry-docker-tests/tests/boto/test_boto_functional.py
@@ -13,23 +13,30 @@
 # limitations under the License.
 
 import unittest
-from opentelemetry.instrumentation.boto import BotoInstrumentor
-from opentelemetry.sdk.trace import NoOpTracerProvider
+
 import boto3
+
+from opentelemetry.instrumentation.botocore import (
+    BotocoreInstrumentor as BotoInstrumentor,
+)
+from opentelemetry.sdk.trace import NoOpTracerProvider
+
 
 class TestBotoInstrumentationNoOpTracerProvider(unittest.TestCase):
     def setUp(self):
         # Set the NoOpTracerProvider
         self.noop_tracer_provider = NoOpTracerProvider()
-        BotoInstrumentor().instrument(tracer_provider=self.noop_tracer_provider)
+        BotoInstrumentor().instrument(
+            tracer_provider=self.noop_tracer_provider
+        )
 
     def tearDown(self):
         BotoInstrumentor().uninstrument()
 
     def test_boto_with_noop_tracer_provider(self):
         # Create a boto3 client
-        client = boto3.client('s3')
-        
+        client = boto3.client("s3")
+
         # Perform a simple operation
         try:
             client.list_buckets()
@@ -37,8 +44,13 @@ class TestBotoInstrumentationNoOpTracerProvider(unittest.TestCase):
             pass  # Ignore any exceptions for this test
 
         # Ensure no spans are created
-        spans = self.noop_tracer_provider.get_tracer("test").get_span_processor().spans
+        spans = (
+            self.noop_tracer_provider.get_tracer("test")
+            .get_span_processor()
+            .spans
+        )
         self.assertEqual(len(spans), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
