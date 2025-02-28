030c93da3d094e3f2dd663b6d472c8aaa6fa5238
Aaron Abbott <aaronabbott@google.com>
2025-02-28 05:27:58 +0000

    Oops, undo accidental change to starlette version

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -181,7 +181,7 @@ libraries = [
         "instrumentation": "opentelemetry-instrumentation-sqlalchemy==0.52b0.dev",
     },
     {
-        "library": "starlette >= 0.13",
+        "library": "starlette >= 0.13, <0.15",
         "instrumentation": "opentelemetry-instrumentation-starlette==0.52b0.dev",
     },
     {
