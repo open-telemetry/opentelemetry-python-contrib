efb5fd0ed55be78db69cdd1176721b67cdc6ffa4
rjduffner <rjduffner@gmail.com>
2025-03-03 12:43:47 -0800

    Add tests around instrumentation_dependencies function

--- instrumentation/opentelemetry-instrumentation-kafka-python/tests/test_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-kafka-python/tests/test_instrumentation.py
@@ -11,13 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from importlib.metadata import PackageNotFoundError
 from unittest import TestCase
+from unittest.mock import patch, call
 
 from kafka import KafkaConsumer, KafkaProducer
 from wrapt import BoundFunctionWrapper
 
 from opentelemetry.instrumentation.kafka import KafkaInstrumentor
-
+from opentelemetry.instrumentation.kafka.package import (
+    _instruments,
+    _instruments_kafka_python,
+    _instruments_kafka_python_ng,
+)
 
 class TestKafka(TestCase):
     def test_instrument_api(self) -> None:
@@ -34,3 +40,80 @@ class TestKafka(TestCase):
         self.assertFalse(
             isinstance(KafkaConsumer.__next__, BoundFunctionWrapper)
         )
+
+    @patch("opentelemetry.instrumentation.kafka.distribution")
+    def test_instrumentation_dependencies_kafka_python_installed(self, mock_distribution) -> None:
+        instrumentation = KafkaInstrumentor()
+
+        def _distribution(name):
+            if name == "kafka-python":
+                return None
+            raise PackageNotFoundError
+
+        mock_distribution.side_effect = _distribution
+        package_to_instrument = instrumentation.instrumentation_dependencies()
+
+        assert mock_distribution.call_count == 2
+        assert mock_distribution.mock_calls == [
+            call("kafka-python-ng"),
+            call("kafka-python"),
+        ]
+        assert package_to_instrument == (_instruments_kafka_python,)
+
+    @patch("opentelemetry.instrumentation.kafka.distribution")
+    def test_instrumentation_dependencies_kafka_python_ng_installed(self, mock_distribution) -> None:
+        instrumentation = KafkaInstrumentor()
+
+        def _distribution(name):
+            if name == "kafka-python-ng":
+                return None
+            raise PackageNotFoundError
+
+        mock_distribution.side_effect = _distribution
+        package_to_instrument = instrumentation.instrumentation_dependencies()
+
+        assert mock_distribution.call_count == 1
+        assert mock_distribution.mock_calls == [
+            call("kafka-python-ng")
+        ]
+        assert package_to_instrument == (_instruments_kafka_python_ng,)
+
+    @patch("opentelemetry.instrumentation.kafka.distribution")
+    def test_instrumentation_dependencies_both_installed(self, mock_distribution) -> None:
+        instrumentation = KafkaInstrumentor()
+
+        def _distribution(name):
+            # Function raises PackageNotFoundError
+            # if name is not in the list. We will
+            # not raise it for both names
+            return None
+
+        mock_distribution.side_effect = _distribution
+        package_to_instrument = instrumentation.instrumentation_dependencies()
+
+        assert mock_distribution.call_count == 1
+        assert mock_distribution.mock_calls == [
+            call("kafka-python-ng")
+        ]
+        assert package_to_instrument == (_instruments_kafka_python_ng,)
+
+    @patch("opentelemetry.instrumentation.kafka.distribution")
+    def test_instrumentation_dependencies_none_installed(self, mock_distribution) -> None:
+        instrumentation = KafkaInstrumentor()
+
+        def _distribution(name):
+            # Function raises PackageNotFoundError
+            # if name is not in the list. We will
+            # raise it for both names to simulate
+            # neither being installed
+            raise PackageNotFoundError
+
+        mock_distribution.side_effect = _distribution
+        package_to_instrument = instrumentation.instrumentation_dependencies()
+
+        assert mock_distribution.call_count == 2
+        assert mock_distribution.mock_calls == [
+            call("kafka-python-ng"),
+            call("kafka-python"),
+        ]
+        assert package_to_instrument == _instruments
