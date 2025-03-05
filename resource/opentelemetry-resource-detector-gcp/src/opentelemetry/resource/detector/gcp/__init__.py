20254a7544290f774d4b269b1160531c3186e097
Dylan Russell <dylanrussell@google.com>
2025-03-05 18:59:51 +0000

    Import the GCP resource detector.

---
+++ resource/opentelemetry-resource-detector-gcp/src/opentelemetry/resource/detector/gcp/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=unused-import
+
+# import GoogleCloudResourceDetector, so it is available in the contrib repo.
+from opentelemetry.resourcedetector.gcp_resource_detector import (
+    GoogleCloudResourceDetector,
+)
