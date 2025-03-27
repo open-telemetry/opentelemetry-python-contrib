fa09ca0fe629023f857b442e0b2c8c0b51e1424c
Andre Murbach Maidl <andremm@gmail.com>
2025-03-27 17:36:41 -0300

    Improve kafka-python instrumentation examples

--- instrumentation/opentelemetry-instrumentation-kafka-python/src/opentelemetry/instrumentation/kafka/__init__.py
+++ instrumentation/opentelemetry-instrumentation-kafka-python/src/opentelemetry/instrumentation/kafka/__init__.py
@@ -57,6 +57,7 @@ for example:
     def produce_hook(span, args, kwargs):
         if span and span.is_recording():
             span.set_attribute("custom_user_attribute_from_produce_hook", "some-value")
+
     def consume_hook(span, record, args, kwargs):
         if span and span.is_recording():
             span.set_attribute("custom_user_attribute_from_consume_hook", "some-value")
@@ -68,6 +69,14 @@ for example:
     # including user custom attributes added from the hooks
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
     producer.send('my-topic', b'raw_bytes')
+
+    def process_msg(message):
+        print(message)
+
+    consumer = KafkaConsumer('my-topic', group_id='my-group', bootstrap_servers=['localhost:9092'])
+    for message in consumer:
+        # process message
+        process_msg(message)
 
 API
 ___
