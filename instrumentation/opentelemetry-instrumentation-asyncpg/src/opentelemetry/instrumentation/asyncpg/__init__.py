4d90332e25aa7672c124c0e02b56155999d85725
Tamara Dancheva <tamara.dancheva@elastic.co>
2025-05-15 11:15:57 -0700

    Replace span in asyncpg

--- instrumentation/opentelemetry-instrumentation-asyncpg/src/opentelemetry/instrumentation/asyncpg/__init__.py
+++ instrumentation/opentelemetry-instrumentation-asyncpg/src/opentelemetry/instrumentation/asyncpg/__init__.py
@@ -60,20 +60,25 @@ from opentelemetry.instrumentation.asyncpg.package import _instruments
 from opentelemetry.instrumentation.asyncpg.version import __version__
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.utils import unwrap
-from opentelemetry.semconv.trace import (
-    DbSystemValues,
-    NetTransportValues,
-    SpanAttributes,
+from opentelemetry.semconv._incubating.attributes.db_attributes import (
+    DB_NAME,
+    DB_STATEMENT,
+    DB_USER,
 )
+from opentelemetry.semconv._incubating.attributes.net_attributes import (
+    NET_PEER_NAME,
+    NET_PEER_PORT,
+    NET_TRANSPORT,
+)
+from opentelemetry.semconv.attributes.db_attributes import DB_SYSTEM_NAME
+from opentelemetry.semconv.trace import DbSystemValues, NetTransportValues
 from opentelemetry.trace import SpanKind
 from opentelemetry.trace.status import Status, StatusCode
 
 
 def _hydrate_span_from_args(connection, query, parameters) -> dict:
     """Get network and database attributes from connection."""
-    span_attributes = {
-        SpanAttributes.DB_SYSTEM: DbSystemValues.POSTGRESQL.value
-    }
+    span_attributes = {DB_SYSTEM_NAME: DbSystemValues.POSTGRESQL.value}
 
     # connection contains _params attribute which is a namedtuple ConnectionParameters.
     # https://github.com/MagicStack/asyncpg/blob/master/asyncpg/connection.py#L68
@@ -81,28 +86,24 @@ def _hydrate_span_from_args(connection, query, parameters) -> dict:
     params = getattr(connection, "_params", None)
     dbname = getattr(params, "database", None)
     if dbname:
-        span_attributes[SpanAttributes.DB_NAME] = dbname
+        span_attributes[DB_NAME] = dbname
     user = getattr(params, "user", None)
     if user:
-        span_attributes[SpanAttributes.DB_USER] = user
+        span_attributes[DB_USER] = user
 
     # connection contains _addr attribute which is either a host/port tuple, or unix socket string
     # https://magicstack.github.io/asyncpg/current/_modules/asyncpg/connection.html
     addr = getattr(connection, "_addr", None)
     if isinstance(addr, tuple):
-        span_attributes[SpanAttributes.NET_PEER_NAME] = addr[0]
-        span_attributes[SpanAttributes.NET_PEER_PORT] = addr[1]
-        span_attributes[SpanAttributes.NET_TRANSPORT] = (
-            NetTransportValues.IP_TCP.value
-        )
+        span_attributes[NET_PEER_NAME] = addr[0]
+        span_attributes[NET_PEER_PORT] = addr[1]
+        span_attributes[NET_TRANSPORT] = NetTransportValues.IP_TCP.value
     elif isinstance(addr, str):
-        span_attributes[SpanAttributes.NET_PEER_NAME] = addr
-        span_attributes[SpanAttributes.NET_TRANSPORT] = (
-            NetTransportValues.OTHER.value
-        )
+        span_attributes[NET_PEER_NAME] = addr
+        span_attributes[NET_TRANSPORT] = NetTransportValues.OTHER.value
 
     if query is not None:
-        span_attributes[SpanAttributes.DB_STATEMENT] = query
+        span_attributes[DB_STATEMENT] = query
 
     if parameters is not None and len(parameters) > 0:
         span_attributes["db.statement.parameters"] = str(parameters)
