28f961f7ef5340574df5ae03b139d2cd316bb468
Radhika Gupta <guptaradhika@microsoft.com>
2025-06-05 10:18:09 -0700

    Updated changelog and moved change to unreleased. Updated test files with license header

--- util/opentelemetry-util-http/tests/test_redact_url.py
+++ util/opentelemetry-util-http/tests/test_redact_url.py
@@ -1,5 +1,19 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
-from opentelemetry.util.http import redact_url, PARAMS_TO_REDACT
+from opentelemetry.util.http import redact_url
 
 class TestRedactUrl(unittest.TestCase):
     def test_redact_both_credentials_and_query_params(self):
@@ -33,7 +47,3 @@ class TestRedactUrl(unittest.TestCase):
         # URL with IP address instead of domain
         url3 = "https://user:pass@192.168.1.1/path?X-Goog-Signature=xyz"
         self.assertEqual(redact_url(url3), "https://REDACTED:REDACTED@192.168.1.1/path?X-Goog-Signature=REDACTED")
-        
-
-if __name__ == "__main__":
-    unittest.main()
