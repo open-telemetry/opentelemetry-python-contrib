38bc22864baa1ba7c18e83578dedde31d490d9bb
Keith Decker <47755047+keith-decker@users.noreply.github.com>
2025-07-22 14:45:29 -0600

    Update scripts/generate_instrumentation_bootstrap.py
    Co-authored-by: Pablo Collins <pablo.collins@gmail.com>

--- scripts/generate_instrumentation_bootstrap.py
+++ scripts/generate_instrumentation_bootstrap.py
@@ -68,7 +68,7 @@ packages_to_exclude = [
     # development. This filter will get removed once it is further along in its
     # development lifecycle and ready to be included by default.
     "opentelemetry-instrumentation-langchain",
-    # Google GenAI instrumentation is currently excluded because it is still in early
+    # Weaviate instrumentation is currently excluded because it is still in early
     # development. This filter will get removed once it is further along in its
     # development lifecycle and ready to be included by default.
     "opentelemetry-instrumentation-weaviate",
