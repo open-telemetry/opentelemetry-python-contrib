8903433251b7375254436dd72b6c7adca67480f4
Wrisa <ridhimasatam@gmail.com>
2025-07-31 18:45:29 -0700

    fixed ruff error

--- instrumentation-genai/opentelemetry-instrumentation-langchain/tests/test_llm_call.py
+++ instrumentation-genai/opentelemetry-instrumentation-langchain/tests/test_llm_call.py
@@ -40,38 +40,63 @@ def test_langchain_call(
         print(f"span attributes: {span.attributes}")
     assert_openai_completion_attributes(spans[0], response)
 
+
 def assert_openai_completion_attributes(
-    span: ReadableSpan,
-    response: Optional
+    span: ReadableSpan, response: Optional
 ):
     assert span.name == "ChatOpenAI.chat"
     assert span.attributes[gen_ai_attributes.GEN_AI_OPERATION_NAME] == "chat"
     assert span.attributes[gen_ai_attributes.GEN_AI_SYSTEM] == "ChatOpenAI"
-    assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_MODEL] == "gpt-3.5-turbo"
-    assert span.attributes[gen_ai_attributes.GEN_AI_RESPONSE_MODEL] == "gpt-3.5-turbo-0125"
+    assert (
+        span.attributes[gen_ai_attributes.GEN_AI_REQUEST_MODEL]
+        == "gpt-3.5-turbo"
+    )
+    assert (
+        span.attributes[gen_ai_attributes.GEN_AI_RESPONSE_MODEL]
+        == "gpt-3.5-turbo-0125"
+    )
     assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_MAX_TOKENS] == 100
     assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_TEMPERATURE] == 0.1
     # TODO: add to semantic conventions
     assert span.attributes["gen_ai.provider.name"] == "openai"
     assert gen_ai_attributes.GEN_AI_RESPONSE_ID in span.attributes
     assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_TOP_P] == 0.9
-    assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_FREQUENCY_PENALTY] == 0.5
-    assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_PRESENCE_PENALTY] == 0.5
-    stop_sequences = span.attributes.get(gen_ai_attributes.GEN_AI_REQUEST_STOP_SEQUENCES)
+    assert (
+        span.attributes[gen_ai_attributes.GEN_AI_REQUEST_FREQUENCY_PENALTY]
+        == 0.5
+    )
+    assert (
+        span.attributes[gen_ai_attributes.GEN_AI_REQUEST_PRESENCE_PENALTY]
+        == 0.5
+    )
+    stop_sequences = span.attributes.get(
+        gen_ai_attributes.GEN_AI_REQUEST_STOP_SEQUENCES
+    )
     assert all(seq in ["\n", "Human:", "AI:"] for seq in stop_sequences)
     assert span.attributes[gen_ai_attributes.GEN_AI_REQUEST_SEED] == 100
 
-    input_tokens = response.response_metadata.get("token_usage").get("prompt_tokens")
+    input_tokens = response.response_metadata.get("token_usage").get(
+        "prompt_tokens"
+    )
     if input_tokens:
-        assert input_tokens == span.attributes[gen_ai_attributes.GEN_AI_USAGE_INPUT_TOKENS]
+        assert (
+            input_tokens
+            == span.attributes[gen_ai_attributes.GEN_AI_USAGE_INPUT_TOKENS]
+        )
     else:
-        assert gen_ai_attributes.GEN_AI_USAGE_INPUT_TOKENS not in span.attributes
+        assert (
+            gen_ai_attributes.GEN_AI_USAGE_INPUT_TOKENS not in span.attributes
+        )
 
-    output_tokens = response.response_metadata.get("token_usage").get("completion_tokens")
+    output_tokens = response.response_metadata.get("token_usage").get(
+        "completion_tokens"
+    )
     if output_tokens:
-        assert output_tokens == span.attributes[gen_ai_attributes.GEN_AI_USAGE_OUTPUT_TOKENS]
+        assert (
+            output_tokens
+            == span.attributes[gen_ai_attributes.GEN_AI_USAGE_OUTPUT_TOKENS]
+        )
     else:
-        assert gen_ai_attributes.GEN_AI_USAGE_OUTPUT_TOKENS not in span.attributes
-
-
-
+        assert (
+            gen_ai_attributes.GEN_AI_USAGE_OUTPUT_TOKENS not in span.attributes
+        )
