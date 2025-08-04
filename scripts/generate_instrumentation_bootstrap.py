edeae262f3bf8dbd3296a7b2311e939f507a5353
Keith Decker <kdecker@splunk.com>
2025-08-04 09:34:46 -0600

    generate bootstrap entry

--- scripts/generate_instrumentation_bootstrap.py
+++ scripts/generate_instrumentation_bootstrap.py
@@ -68,6 +68,10 @@ packages_to_exclude = [
     # development. This filter will get removed once it is further along in its
     # development lifecycle and ready to be included by default.
     "opentelemetry-instrumentation-langchain",
+    # GenAI SDK is currently excluded because it is still in early
+    # development. This filter will get removed once it is further along in its
+    # development lifecycle and ready to be included by default.
+    "opentelemetry-genai-sdk",
 ]
 
 # Static version specifiers for instrumentations that are released independently
