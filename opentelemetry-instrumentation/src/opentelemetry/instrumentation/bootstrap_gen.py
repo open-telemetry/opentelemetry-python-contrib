9587b3fcc5c22f1428357a462c9d26a9f9473b02
Keith Decker <kdecker@splunk.com>
2025-08-07 17:08:58 -0600

    move package out of instrumentation folder

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -214,7 +214,6 @@ libraries = [
     },
 ]
 default_instrumentations = [
-    "opentelemetry-genai-utils==0.1b0.dev",
     "opentelemetry-instrumentation-asyncio==0.58b0.dev",
     "opentelemetry-instrumentation-dbapi==0.58b0.dev",
     "opentelemetry-instrumentation-logging==0.58b0.dev",
