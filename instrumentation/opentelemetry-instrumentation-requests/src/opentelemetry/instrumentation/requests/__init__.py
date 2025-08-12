d3b211ae7a92d491b6505d5a5e467ec9e79e243b
Amit Bhushan <amitbhushan@192.168.1.12>
2025-08-12 23:22:11 +0530

    test--just hostname in spanname of request

--- instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
+++ instrumentation/opentelemetry-instrumentation-requests/src/opentelemetry/instrumentation/requests/__init__.py
@@ -232,7 +232,11 @@ def _instrument(
         method = request.method
         span_name = get_default_span_name(method)
         url = remove_url_credentials(request.url)
-        span_name = f"{span_name} {url}"
+        parsed_url = urlparse(url)
+        hostname = parsed_url.hostname
+        span_name = f"{span_name} {hostname}"
+
+
 
         span_attributes = {}
         _set_http_method(
