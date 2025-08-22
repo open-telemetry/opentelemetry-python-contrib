1227caf208932e7f03048443e252741fa05d2dba
Liudmila Molkova <neskazu@gmail.com>
2025-08-22 16:04:33 -0700

    Update versions and enabled metrics in openai examples

--- instrumentation-genai/opentelemetry-instrumentation-openai-v2/examples/manual/main.py
+++ instrumentation-genai/opentelemetry-instrumentation-openai-v2/examples/manual/main.py
@@ -4,12 +4,15 @@ import os
 from openai import OpenAI
 
 # NOTE: OpenTelemetry Python Logs and Events APIs are in beta
-from opentelemetry import _events, _logs, trace
+from opentelemetry import _events, _logs, metrics, trace
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
     OTLPLogExporter,
 )
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
+)
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
+    OTLPMetricExporter,
 )
 from opentelemetry.instrumentation.openai_v2 import OpenAIInstrumentor
 from opentelemetry.sdk._events import EventLoggerProvider
@@ -17,6 +20,9 @@ from opentelemetry.sdk._logs import LoggerProvider
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
 # configure tracing
 trace.set_tracer_provider(TracerProvider())
@@ -30,6 +36,18 @@ _logs.get_logger_provider().add_log_record_processor(
     BatchLogRecordProcessor(OTLPLogExporter())
 )
 _events.set_event_logger_provider(EventLoggerProvider())
+
+# configure metrics
+metrics.set_meter_provider(MeterProvider(
+    metric_readers=[
+        PeriodicExportingMetricReader(
+            OTLPMetricExporter(),
+        ),
+    ]
+))
+
+from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+HTTPXClientInstrumentor().instrument()
 
 # instrument OpenAI
 OpenAIInstrumentor().instrument()
