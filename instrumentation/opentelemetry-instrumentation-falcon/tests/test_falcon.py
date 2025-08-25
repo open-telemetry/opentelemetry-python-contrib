e50f1e24ae1c74e5703294c24d2a7f44dc5a1aea
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2025-08-25 15:21:24 -0700

    disable too-many-lints

--- instrumentation/opentelemetry-instrumentation-falcon/tests/test_falcon.py
+++ instrumentation/opentelemetry-instrumentation-falcon/tests/test_falcon.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# pylint: disable=too-many-lines
+
 from timeit import default_timer
 from unittest.mock import Mock, patch
 
