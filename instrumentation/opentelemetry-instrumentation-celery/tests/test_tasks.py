90666fca03fdfb4f72069ae2072c87ce2fad5396
mriamah <mriamah1@outlook.com>
2025-08-27 13:29:58 +0100

    Update celery instrumention tests to follow semantic conventions

--- instrumentation/opentelemetry-instrumentation-celery/tests/test_tasks.py
+++ instrumentation/opentelemetry-instrumentation-celery/tests/test_tasks.py
@@ -20,7 +20,12 @@ from wrapt import wrap_function_wrapper
 from opentelemetry import baggage, context
 from opentelemetry.instrumentation.celery import CeleryInstrumentor, utils
 from opentelemetry.instrumentation.utils import unwrap
-from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.semconv._incubating.attributes import (
+    exception_attributes as ExceptionAttributes,
+)
+from opentelemetry.semconv._incubating.attributes import (
+    messaging_attributes as SpanAttributes,
+)
 from opentelemetry.test.test_base import TestBase
 from opentelemetry.trace import SpanKind, StatusCode
 
@@ -65,7 +70,7 @@ class TestCeleryInstrumentation(TestBase):
             {
                 "celery.action": "run",
                 "celery.state": "SUCCESS",
-                SpanAttributes.MESSAGING_DESTINATION: "celery",
+                SpanAttributes.MESSAGING_DESTINATION_NAME: "celery",
                 "celery.task_name": "tests.celery_test_tasks.task_add",
             },
         )
@@ -83,8 +88,7 @@ class TestCeleryInstrumentation(TestBase):
             {
                 "celery.action": "apply_async",
                 "celery.task_name": "tests.celery_test_tasks.task_add",
-                SpanAttributes.MESSAGING_DESTINATION_KIND: "queue",
-                SpanAttributes.MESSAGING_DESTINATION: "celery",
+                SpanAttributes.MESSAGING_DESTINATION_NAME: "celery",
             },
         )
 
@@ -117,7 +121,7 @@ class TestCeleryInstrumentation(TestBase):
             {
                 "celery.action": "run",
                 "celery.state": "FAILURE",
-                SpanAttributes.MESSAGING_DESTINATION: "celery",
+                SpanAttributes.MESSAGING_DESTINATION_NAME: "celery",
                 "celery.task_name": "tests.celery_test_tasks.task_raises",
             },
         )
@@ -127,15 +131,17 @@ class TestCeleryInstrumentation(TestBase):
         self.assertEqual(1, len(consumer.events))
         event = consumer.events[0]
 
-        self.assertIn(SpanAttributes.EXCEPTION_STACKTRACE, event.attributes)
+        self.assertIn(
+            ExceptionAttributes.EXCEPTION_STACKTRACE, event.attributes
+        )
 
         # TODO: use plain assertEqual after 1.25 is released (https://github.com/open-telemetry/opentelemetry-python/pull/3837)
         self.assertIn(
-            "CustomError", event.attributes[SpanAttributes.EXCEPTION_TYPE]
+            "CustomError", event.attributes[ExceptionAttributes.EXCEPTION_TYPE]
         )
 
         self.assertEqual(
-            event.attributes[SpanAttributes.EXCEPTION_MESSAGE],
+            event.attributes[ExceptionAttributes.EXCEPTION_MESSAGE],
             "The task failed!",
         )
 
@@ -148,8 +154,7 @@ class TestCeleryInstrumentation(TestBase):
             {
                 "celery.action": "apply_async",
                 "celery.task_name": "tests.celery_test_tasks.task_raises",
-                SpanAttributes.MESSAGING_DESTINATION_KIND: "queue",
-                SpanAttributes.MESSAGING_DESTINATION: "celery",
+                SpanAttributes.MESSAGING_DESTINATION_NAME: "celery",
             },
         )
 
