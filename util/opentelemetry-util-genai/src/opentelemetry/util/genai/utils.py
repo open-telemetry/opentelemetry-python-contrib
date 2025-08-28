ec423392f3a15e55bacb263b3d1ca469f9547513
Dylan Russell <dylanrus@gmail.com>
2025-08-28 13:56:22 +0000

    initial commit

---
+++ util/opentelemetry-util-genai/src/opentelemetry/util/genai/utils.py
@@ -0,0 +1,48 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from opentelemetry.instrumentation._semconv import (
+    _OpenTelemetrySemanticConventionStability,
+    _OpenTelemetryStabilitySignalType,
+    _StabilityMode,
+)
+from opentelemetry.util.genai.environment_variables import (
+    OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT,
+)
+from opentelemetry.util.genai.types import ContentCapturingMode
+
+
+def get_content_capturing_mode() -> ContentCapturingMode:
+    envvar = os.environ.get(OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT)
+    if (
+        _OpenTelemetrySemanticConventionStability._get_opentelemetry_stability_opt_in_mode(
+            _OpenTelemetryStabilitySignalType.GEN_AI,
+        )
+        == _StabilityMode.DEFAULT
+    ):
+        raise RuntimeError(
+            "This function should never be called when StabilityMode is default.."
+        )
+    if not envvar:
+        return ContentCapturingMode.NO_CONTENT
+    try:
+        return ContentCapturingMode[envvar.upper()]
+    except KeyError:
+        raise RuntimeError(
+            "{} is not a valid option for `OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT` environment variable. Must be one of {}".format(
+                envvar, ", ".join([e.name for e in ContentCapturingMode])
+            )
+        )
---
+++ util/opentelemetry-util-genai/tests/test_utils.py
@@ -0,0 +1,74 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+from unittest.mock import patch
+
+from opentelemetry.instrumentation._semconv import (
+    OTEL_SEMCONV_STABILITY_OPT_IN,
+    _OpenTelemetrySemanticConventionStability,
+)
+from opentelemetry.util.genai.environment_variables import (
+    OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT,
+)
+from opentelemetry.util.genai.types import ContentCapturingMode
+from opentelemetry.util.genai.utils import get_content_capturing_mode
+
+
+def patch_env_vars(stability_mode, content_capturing):
+    def decorator(test_case):
+        @patch.dict(
+            os.environ,
+            {
+                OTEL_SEMCONV_STABILITY_OPT_IN: stability_mode,
+                OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT: content_capturing,
+            },
+        )
+        def wrapper(*args, **kwargs):
+            # Reset state.
+            _OpenTelemetrySemanticConventionStability._initialized = False
+            _OpenTelemetrySemanticConventionStability._initialize()
+            return test_case(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+class TestVersion(unittest.TestCase):
+    @patch_env_vars("gen_ai_latest_experimental", "SPAN_ONLY")
+    def test_get_content_caputring_mode_parses_valid_envvar(self):  # pylint: disable=no-self-use
+        _OpenTelemetrySemanticConventionStability._initialized = False
+        assert get_content_capturing_mode() == ContentCapturingMode.SPAN_ONLY
+
+    @patch_env_vars("gen_ai_latest_experimental", "")
+    def test_empty_content_capturing_envvar(self):  # pylint: disable=no-self-use
+        _OpenTelemetrySemanticConventionStability._initialized = False
+        assert get_content_capturing_mode() == ContentCapturingMode.NO_CONTENT
+
+    @patch_env_vars("default", "True")
+    def test_get_content_caputring_mode_raises_exception_when_semconv_stability_default(
+        self,
+    ):  # pylint: disable=no-self-use
+        _OpenTelemetrySemanticConventionStability._initialized = False
+        with self.assertRaises(RuntimeError):
+            get_content_capturing_mode()
+
+    @patch_env_vars("gen_ai_latest_experimental", "INVALID_VALUE")
+    def test_get_content_caputring_mode_raises_exception_on_invalid_envvar(
+        self,
+    ):  # pylint: disable=no-self-use
+        with self.assertRaises(RuntimeError):
+            get_content_capturing_mode()
