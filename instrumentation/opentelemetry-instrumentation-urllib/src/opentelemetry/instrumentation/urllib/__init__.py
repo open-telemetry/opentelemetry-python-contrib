99280f1cf0cb7f3482a7f8623302d514255d11b6
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-12-24 09:26:06 +0100

    Apply suggestions from code review
    Co-authored-by: Lukas Hering <40302054+herin049@users.noreply.github.com>

--- instrumentation/opentelemetry-instrumentation-urllib/src/opentelemetry/instrumentation/urllib/__init__.py
+++ instrumentation/opentelemetry-instrumentation-urllib/src/opentelemetry/instrumentation/urllib/__init__.py
@@ -94,7 +94,7 @@ For example using the environment variable,
 
 will extract ``content-type`` and ``custom_request_header`` from the request headers and add them as span attributes.
 
-Request header names in aiohttp are case-insensitive. So, giving the header name as ``CUStom-Header`` in the environment
+Request header names in urllib are case-insensitive. So, giving the header name as ``CUStom-Header`` in the environment
 variable will capture the header named ``custom-header``.
 
 Regular expressions may also be used to match multiple headers that correspond to the given pattern.  For example:
@@ -128,7 +128,7 @@ For example using the environment variable,
 
 will extract ``content-type`` and ``custom_response_header`` from the response headers and add them as span attributes.
 
-Response header names in aiohttp are case-insensitive. So, giving the header name as ``CUStom-Header`` in the environment
+Response header names in urllib are case-insensitive. So, giving the header name as ``CUStom-Header`` in the environment
 variable will capture the header named ``custom-header``.
 
 Regular expressions may also be used to match multiple headers that correspond to the given pattern.  For example:
@@ -167,8 +167,6 @@ will replace the value of headers such as ``session-id`` and ``set-cookie`` with
 
 Note:
     The environment variable names used to capture HTTP headers are still experimental, and thus are subject to change.
-
-
 
 API
 ---
