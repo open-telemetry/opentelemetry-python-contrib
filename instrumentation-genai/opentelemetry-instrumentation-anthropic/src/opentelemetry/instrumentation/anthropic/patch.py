b6d448a871c27e73fb7dec1d90294b751135448e
Teja <vasant.teja@gmail.com>
2025-12-24 23:29:33 -0500

    wip: fixing tox issues.

--- instrumentation-genai/opentelemetry-instrumentation-anthropic/src/opentelemetry/instrumentation/anthropic/patch.py
+++ instrumentation-genai/opentelemetry-instrumentation-anthropic/src/opentelemetry/instrumentation/anthropic/patch.py
@@ -42,7 +42,7 @@ def messages_create(
         attributes = get_llm_request_attributes(kwargs, instance)
         request_model = attributes.get(
             GenAIAttributes.GEN_AI_REQUEST_MODEL
-        ) or kwargs.get("model")
+        ) or kwargs.get("model") or "unknown"
 
         invocation = LLMInvocation(
             request_model=request_model,
