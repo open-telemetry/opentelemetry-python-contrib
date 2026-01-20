642c4e5da8ea3c17a565fa53e700efd814f958cf
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-01-20 11:11:10 +0100

    Update __init__.py

--- instrumentation/opentelemetry-instrumentation-pymemcache/src/opentelemetry/instrumentation/pymemcache/__init__.py
+++ instrumentation/opentelemetry-instrumentation-pymemcache/src/opentelemetry/instrumentation/pymemcache/__init__.py
@@ -185,8 +185,10 @@ def _get_address_attributes(instance):
 
 class PymemcacheInstrumentor(BaseInstrumentor):
     """An instrumentor for pymemcache See `BaseInstrumentor`"""
+
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
+
     def _instrument(self, **kwargs):
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(
@@ -202,6 +204,7 @@ class PymemcacheInstrumentor(BaseInstrumentor):
                 f"Client.{cmd}",
                 _wrap_cmd(tracer, cmd),
             )
+
     def _uninstrument(self, **kwargs):
         for command in COMMANDS:
             unwrap(pymemcache.client.base.Client, f"{command}")
