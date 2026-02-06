faadf1780879d66e76eb658eee658ca15d3d48a3
wikaaaaa <wikiwalczak6@gmail.com>
2026-02-06 10:33:57 +0000

    add gen_ai.tool_definitions to completion hook

--- instrumentation-genai/opentelemetry-instrumentation-google-genai/src/opentelemetry/instrumentation/google_genai/generate_content.py
+++ instrumentation-genai/opentelemetry-instrumentation-google-genai/src/opentelemetry/instrumentation/google_genai/generate_content.py
@@ -611,7 +611,7 @@ class _GenerateContentInstrumentationHelper:
         request: Union[ContentListUnion, ContentListUnionDict],
         candidates: list[Candidate],
         config: Optional[GenerateContentConfigOrDict] = None,
-        tool_definitions: list[MessagePart] = None,
+        tool_definitions: list[MessagePart] = [],
     ):
         if (
             self.sem_conv_opt_in_mode
