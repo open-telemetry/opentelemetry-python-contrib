5b6b52fb2fd30f7b810c6fc9d8064ecd52646e91
42Questions <michael.summerville@gmail.com>
2026-02-07 14:25:55 +0000

    Added _instrumentation in package.py pattern for aio_pika

--- instrumentation/opentelemetry-instrumentation-aio-pika/tests/test_aio_pika_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-aio-pika/tests/test_aio_pika_instrumentation.py
@@ -11,12 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest import TestCase
+from unittest import TestCase, mock
 
 import wrapt
 from aio_pika import Exchange, Queue
 
 from opentelemetry.instrumentation.aio_pika import AioPikaInstrumentor
+from opentelemetry.instrumentation.aio_pika.package import (
+    _instrumentation_name,
+)
 
 
 class TestPika(TestCase):
@@ -32,3 +35,13 @@ class TestPika(TestCase):
         self.assertFalse(
             isinstance(Exchange.publish, wrapt.BoundFunctionWrapper)
         )
+
+    def test_instrumentation_name(self) -> None:
+        with mock.patch("opentelemetry.trace.get_tracer") as mock_get_tracer:
+            instrumentation = AioPikaInstrumentor()
+            instrumentation.instrument()
+            mock_get_tracer.assert_called_once()
+            self.assertEqual(
+                mock_get_tracer.call_args[0][0], _instrumentation_name
+            )
+            instrumentation.uninstrument()
