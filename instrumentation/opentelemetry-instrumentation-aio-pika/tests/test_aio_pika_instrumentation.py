004da7ef7c903d9ff02204aa35d033327d88edc2
42Questions <michael.summerville@gmail.com>
2026-02-09 11:53:35 +0000

    Changed test to use TestBase

--- instrumentation/opentelemetry-instrumentation-aio-pika/tests/test_aio_pika_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-aio-pika/tests/test_aio_pika_instrumentation.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import asyncio
 from unittest import TestCase, mock
 
 import wrapt
@@ -19,6 +20,17 @@ from aio_pika import Exchange, Queue
 from opentelemetry.instrumentation.aio_pika import AioPikaInstrumentor
 from opentelemetry.instrumentation.aio_pika.package import (
     _instrumentation_name,
+)
+from opentelemetry.test.test_base import TestBase
+
+from .consts import (
+    AIOPIKA_VERSION_INFO,
+    CHANNEL_7,
+    CHANNEL_8,
+    CONNECTION_7,
+    EXCHANGE_NAME,
+    MESSAGE,
+    ROUTING_KEY,
 )
 
 
@@ -36,12 +48,43 @@ class TestPika(TestCase):
             isinstance(Exchange.publish, wrapt.BoundFunctionWrapper)
         )
 
-    def test_instrumentation_name(self) -> None:
-        with mock.patch("opentelemetry.trace.get_tracer") as mock_get_tracer:
-            instrumentation = AioPikaInstrumentor()
-            instrumentation.instrument()
-            mock_get_tracer.assert_called_once()
-            self.assertEqual(
-                mock_get_tracer.call_args[0][0], _instrumentation_name
+
+class TestInstrumentationScopeName(TestBase):
+    """Test instrumentation scope via actual span output (not mocking internals)."""
+
+    def setUp(self):
+        super().setUp()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        with self.disable_logging():
+            AioPikaInstrumentor().uninstrument()
+        self.loop.close()
+        super().tearDown()
+
+    def test_instrumentation_scope_name(self):
+        """Verify instrumentation scope via the real instrument() path."""
+        async def _noop_publish(self, *args, **kwargs):
+            return None
+
+        with mock.patch.object(Exchange, "publish", new=_noop_publish):
+            AioPikaInstrumentor().instrument(
+                tracer_provider=self.tracer_provider
             )
-            instrumentation.uninstrument()
+            major = AIOPIKA_VERSION_INFO[0]
+            if major == 7:
+                exchange = Exchange(CONNECTION_7, CHANNEL_7, EXCHANGE_NAME)
+            elif major == 8:
+                exchange = Exchange(CHANNEL_8, EXCHANGE_NAME)
+            else:
+                self.fail(f"Unsupported aio-pika version: {AIOPIKA_VERSION_INFO}")
+            self.loop.run_until_complete(
+                exchange.publish(MESSAGE, ROUTING_KEY)
+            )
+
+        spans = self.memory_exporter.get_finished_spans()
+        self.assertEqual(len(spans), 1)
+        self.assertEqual(
+            spans[0].instrumentation_scope.name, _instrumentation_name
+        )
