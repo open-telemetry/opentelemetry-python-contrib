88e5e79843ae58e638a031467165773660325eb5
srikaaviya <krsrikaaviya3@gmail.com>
2026-02-25 07:35:51 -0800

    asyncio: fix environment variables not in docs
    Fix the docstrings in environment_variables.py so they are correctly
    picked up by Sphinx autodoc for Read the Docs.
    
    Previously the docstrings were placed above the variable assignments,
    which Sphinx cannot parse. Moved them below and added the
    required '.. envvar::' directives, consistent with how other
    packages (e.g. opentelemetry-instrumentation) document their
    environment variables.
    
    Fixes #4256

--- instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/environment_variables.py
+++ instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/environment_variables.py
@@ -12,24 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Enter the names of the coroutines to be traced through the environment variable below, separated by commas.
-"""
-
 OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE = (
     "OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE"
 )
+"""
+.. envvar:: OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE
 
+Enter the names of the coroutines to be traced through the environment variable below, separated by commas.
 """
-To determines whether the tracing feature for Future of Asyncio in Python is enabled or not.
-"""
+
 OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED = (
     "OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED"
 )
+"""
+.. envvar:: OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED
 
+To determines whether the tracing feature for Future of Asyncio in Python is enabled or not.
 """
-Enter the names of the functions to be traced through the environment variable below, separated by commas.
-"""
+
 OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE = (
     "OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE"
 )
+"""
+.. envvar:: OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE
+
+Enter the names of the functions to be traced through the environment variable below, separated by commas.
+"""
