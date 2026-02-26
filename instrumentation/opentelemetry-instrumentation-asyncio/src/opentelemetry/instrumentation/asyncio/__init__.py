2c47322b01b20287067803426506f1f7a1beae41
srikaaviya <krsrikaaviya3@gmail.com>
2026-02-26 15:19:43 -0800

    asyncio: move environment_variables docs to __init__ module docstring for sphinx

--- instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/__init__.py
+++ instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/__init__.py
@@ -80,6 +80,22 @@ asyncio metric types
 * asyncio.process.count (count) - Number of asyncio process
 
 
+
+Environment variables
+---------------------
+
+.. envvar:: OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE
+
+Enter the names of the coroutines to be traced through the environment variable below, separated by commas.
+
+.. envvar:: OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED
+
+To determine whether the tracing feature for Future of Asyncio in Python is enabled or not.
+
+.. envvar:: OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE
+
+Enter the names of the functions to be traced through the environment variable below, separated by commas.
+
 API
 ---
 """
--- instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/environment_variables.py
+++ instrumentation/opentelemetry-instrumentation-asyncio/src/opentelemetry/instrumentation/asyncio/environment_variables.py
@@ -15,26 +15,11 @@
 OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE = (
     "OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE"
 )
-"""
-.. envvar:: OTEL_PYTHON_ASYNCIO_COROUTINE_NAMES_TO_TRACE
-
-Enter the names of the coroutines to be traced through the environment variable below, separated by commas.
-"""
 
 OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED = (
     "OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED"
 )
-"""
-.. envvar:: OTEL_PYTHON_ASYNCIO_FUTURE_TRACE_ENABLED
-
-To determine whether the tracing feature for Future of Asyncio in Python is enabled or not.
-"""
 
 OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE = (
     "OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE"
 )
-"""
-.. envvar:: OTEL_PYTHON_ASYNCIO_TO_THREAD_FUNCTION_NAMES_TO_TRACE
-
-Enter the names of the functions to be traced through the environment variable below, separated by commas.
-"""
