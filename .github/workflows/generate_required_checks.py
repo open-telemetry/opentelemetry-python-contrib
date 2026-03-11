f7aabfedd1e264cefa078e7c0239b4a7230ce365
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-03-11 12:27:17 +0100

    Serialize also misc jobs

--- .github/workflows/generate_required_checks.py
+++ .github/workflows/generate_required_checks.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from generate_workflows_lib import (
     get_lint_job_datas,
+    get_misc_job_datas,
     get_test_job_datas,
     get_tox_envs,
 )
@@ -10,8 +11,14 @@ tox_ini_path = Path(__file__).parent.parent.parent.joinpath("tox.ini")
 
 
 def get_gh_contexts_from_jobs(job_datas):
+    def get_job_name(job):
+        if isinstance(job, str):
+            return job
+        return job["ui_name"]
+
     return [
-        f"""      {{ context = "{job["ui_name"]}" }},\n""" for job in job_datas
+        f"""      {{ context = "{get_job_name(job)}" }},\n"""
+        for job in job_datas
     ]
 
 
@@ -20,6 +27,7 @@ jobs = sorted(
     + get_gh_contexts_from_jobs(
         get_test_job_datas(get_tox_envs(tox_ini_path), ["ubuntu-latest"])
     )
+    + get_gh_contexts_from_jobs(get_misc_job_datas(get_tox_envs(tox_ini_path)))
 )
 with open("opentelemetry-admin-jobs.txt", "w") as f:
     for job in jobs:
