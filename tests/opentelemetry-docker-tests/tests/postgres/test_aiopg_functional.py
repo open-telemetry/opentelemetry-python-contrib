120d32c00b720a7f09d4a3ad2f06830b2a20f7f2
RiyaChaturvedi37 <chaturvediriya37@gmail.com>
2026-03-17 22:53:05 +0530

    refactor(docker-tests): replace SpanAttributes with semconv attributes
    Refs #3475

--- tests/opentelemetry-docker-tests/tests/postgres/test_aiopg_functional.py
+++ tests/opentelemetry-docker-tests/tests/postgres/test_aiopg_functional.py
@@ -20,7 +20,15 @@ import pytest
 
 from opentelemetry import trace as trace_api
 from opentelemetry.instrumentation.aiopg import AiopgInstrumentor
-from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.semconv._incubating.attributes.db_attributes import (
+    DB_NAME,
+    DB_SYSTEM,
+    DB_USER,
+)
+from opentelemetry.semconv._incubating.attributes.net_attributes import (
+    NET_PEER_NAME,
+    NET_PEER_PORT,
+)
 from opentelemetry.test.test_base import TestBase
 
 POSTGRES_HOST = os.getenv("POSTGRESQL_HOST", "127.0.0.1")
@@ -75,19 +83,19 @@ class TestFunctionalAiopgConnect(TestBase):
         self.assertIs(child_span.parent, root_span.get_span_context())
         self.assertIs(child_span.kind, trace_api.SpanKind.CLIENT)
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_SYSTEM], "postgresql"
+            child_span.attributes[DB_SYSTEM], "postgresql"
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_NAME], POSTGRES_DB_NAME
+            child_span.attributes[DB_NAME], POSTGRES_DB_NAME
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_USER], POSTGRES_USER
+            child_span.attributes[DB_USER], POSTGRES_USER
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.NET_PEER_NAME], POSTGRES_HOST
+            child_span.attributes[NET_PEER_NAME], POSTGRES_HOST
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.NET_PEER_PORT], POSTGRES_PORT
+            child_span.attributes[NET_PEER_PORT], POSTGRES_PORT
         )
 
     def test_execute(self):
@@ -162,19 +170,19 @@ class TestFunctionalAiopgCreatePool(TestBase):
         self.assertIs(child_span.parent, root_span.get_span_context())
         self.assertIs(child_span.kind, trace_api.SpanKind.CLIENT)
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_SYSTEM], "postgresql"
+            child_span.attributes[DB_SYSTEM], "postgresql"
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_NAME], POSTGRES_DB_NAME
+            child_span.attributes[DB_NAME], POSTGRES_DB_NAME
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.DB_USER], POSTGRES_USER
+            child_span.attributes[DB_USER], POSTGRES_USER
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.NET_PEER_NAME], POSTGRES_HOST
+            child_span.attributes[NET_PEER_NAME], POSTGRES_HOST
         )
         self.assertEqual(
-            child_span.attributes[SpanAttributes.NET_PEER_PORT], POSTGRES_PORT
+            child_span.attributes[NET_PEER_PORT], POSTGRES_PORT
         )
 
     def test_execute(self):
