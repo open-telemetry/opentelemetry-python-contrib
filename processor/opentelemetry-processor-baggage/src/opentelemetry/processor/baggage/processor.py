f90ab6c7852e5d90c65d0e03306f2ee927fb9d51
manvi <manvigrnoida24@gmail.com>
2026-04-09 21:10:41 +0530

    Update BaggageSpanProcessor to support multiple predicates for consistency

--- processor/opentelemetry-processor-baggage/src/opentelemetry/processor/baggage/processor.py
+++ processor/opentelemetry-processor-baggage/src/opentelemetry/processor/baggage/processor.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Optional
+from typing import Callable, Optional, Sequence, Union
 
 from opentelemetry.baggage import get_all as get_all_baggage
 from opentelemetry.context import Context
@@ -21,6 +21,9 @@ from opentelemetry.trace import Span
 
 # A BaggageKeyPredicate is a function that takes a baggage key and returns a boolean
 BaggageKeyPredicateT = Callable[[str], bool]
+BaggageKeyPredicates = Union[
+    BaggageKeyPredicateT, Sequence[BaggageKeyPredicateT]
+]
 
 # A BaggageKeyPredicate that always returns True, allowing all baggage keys to be added to spans
 ALLOW_ALL_BAGGAGE_KEYS: BaggageKeyPredicateT = lambda _: True  # noqa: E731
@@ -50,13 +53,16 @@ class BaggageSpanProcessor(SpanProcessor):
 
     """
 
-    def __init__(self, baggage_key_predicate: BaggageKeyPredicateT) -> None:
-        self._baggage_key_predicate = baggage_key_predicate
+    def __init__(self, baggage_key_predicate: BaggageKeyPredicates) -> None:
+        if callable(baggage_key_predicate):
+            self._predicates = [baggage_key_predicate]
+        else:
+            self._predicates = list(baggage_key_predicate)
 
     def on_start(
         self, span: "Span", parent_context: Optional[Context] = None
     ) -> None:
         baggage = get_all_baggage(parent_context)
         for key, value in baggage.items():
-            if self._baggage_key_predicate(key):
+            if any(predicate(key) for predicate in self._predicates):
                 span.set_attribute(key, value)
