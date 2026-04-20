c17607d97883f5b1d7a8857791addff18f3383c6
wrisa <ridhimasatam@gmail.com>
2026-04-20 11:55:18 -0700

    fixed error

--- instrumentation-genai/opentelemetry-instrumentation-langchain/src/opentelemetry/instrumentation/langchain/callback_handler.py
+++ instrumentation-genai/opentelemetry-instrumentation-langchain/src/opentelemetry/instrumentation/langchain/callback_handler.py
@@ -77,8 +77,9 @@ class OpenTelemetryLangChainCallbackHandler(BaseCallbackHandler):
             self._invocation_manager.add_invocation_state(run_id, None, wf)
             return
         else:
+            # TODO: For agent invocation
             self._invocation_manager.add_invocation_state(
-                run_id, parent_run_id
+                run_id, parent_run_id,
             )
 
     def on_chain_end(
