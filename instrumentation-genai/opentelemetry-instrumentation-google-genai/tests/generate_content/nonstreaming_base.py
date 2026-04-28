122daf072bcf286b5a67afd89bd3c61794a67b1e
Rima Al Ghossein <ralghossein@google.com>
2026-04-28 22:31:10 +0200

    fix tests

--- instrumentation-genai/opentelemetry-instrumentation-google-genai/tests/generate_content/nonstreaming_base.py
+++ instrumentation-genai/opentelemetry-instrumentation-google-genai/tests/generate_content/nonstreaming_base.py
@@ -261,18 +261,22 @@ class NonStreamingTestCase(TestCase):
 
     def test_generated_span_counts_tokens(self):
         self.configure_valid_response(
-            input_tokens=123, output_tokens=456, thoughts_tokens=789, cached_tokens=50
+            input_tokens=123,
+            candidate_tokens=456,
+            thoughts_tokens=789,
+            cached_tokens=50,
         )
         self.generate_content(model="gemini-2.0-flash", contents="Some input")
         self.otel.assert_has_span_named("generate_content gemini-2.0-flash")
         span = self.otel.get_span_named("generate_content gemini-2.0-flash")
         self.assertEqual(span.attributes["gen_ai.usage.input_tokens"], 123)
-        self.assertEqual(span.attributes["gen_ai.usage.output_tokens"], 1245)
+        self.assertEqual(
+            span.attributes["gen_ai.usage.output_tokens"], 456 + 789
+        )
         # New sem conv should not appear when flag is not experimental mode..
         self.assertNotIn(
             "gen_ai.usage.cache_read.input_tokens", span.attributes
         )
-
 
     @patch.dict(
         "os.environ",
