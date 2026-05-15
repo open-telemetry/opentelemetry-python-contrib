3f24ece3f0174990eaa9752a2bad9571e836f431
Zhikuan Wei <zwei129@bloomberg.net>
2026-05-14 22:33:01 -0400

    add handler with response body to unit tests

--- instrumentation/opentelemetry-instrumentation-aiohttp-client/tests/test_aiohttp_client_integration.py
+++ instrumentation/opentelemetry-instrumentation-aiohttp-client/tests/test_aiohttp_client_integration.py
@@ -1217,6 +1217,10 @@ class TestAioHttpClientInstrumentor(TestBase):
         return aiohttp.web.Response(status=200)
 
     @staticmethod
+    async def handler_with_body(request):
+        return aiohttp.web.Response(status=200, body=b"hello")
+
+    @staticmethod
     def get_default_request(url: str = URL):
         async def default_request(server: aiohttp.test_utils.TestServer):
             async with aiohttp.test_utils.TestClient(server) as session:
@@ -1519,7 +1523,7 @@ class TestAioHttpClientInstrumentor(TestBase):
         AioHttpClientInstrumentor().instrument()
 
         run_with_test_server(
-            self.get_default_request(), self.URL, self.default_handler
+            self.get_default_request(), self.URL, self.handler_with_body
         )
         span = self._assert_spans(1)
         self.assertNotIn(HTTP_RESPONSE_BODY_SIZE, span.attributes)
@@ -1535,7 +1539,7 @@ class TestAioHttpClientInstrumentor(TestBase):
         AioHttpClientInstrumentor().uninstrument()
         AioHttpClientInstrumentor().instrument()
         run_with_test_server(
-            self.get_default_request(), self.URL, self.default_handler
+            self.get_default_request(), self.URL, self.handler_with_body
         )
         span = self._assert_spans(1)
         self.assertIn(HTTP_RESPONSE_BODY_SIZE, span.attributes)
@@ -1551,8 +1555,9 @@ class TestAioHttpClientInstrumentor(TestBase):
     def test_response_body_size_metric_recorded(self):
         AioHttpClientInstrumentor().uninstrument()
         AioHttpClientInstrumentor().instrument()
+
         run_with_test_server(
-            self.get_default_request(), self.URL, self.default_handler
+            self.get_default_request(), self.URL, self.handler_with_body
         )
         metrics = self._assert_metrics(2)
         metric_names = {m.name for m in metrics}
@@ -1574,7 +1579,7 @@ class TestAioHttpClientInstrumentor(TestBase):
         AioHttpClientInstrumentor().uninstrument()
         AioHttpClientInstrumentor().instrument()
         run_with_test_server(
-            self.get_default_request(), self.URL, self.default_handler
+            self.get_default_request(), self.URL, self.handler_with_body
         )
         span = self._assert_spans(1)
         self.assertNotIn(HTTP_RESPONSE_BODY_SIZE, span.attributes)
