6d6505db48da81c2a2764199121d4e20975a603e
Mergepath <mergepath@local>
2026-08-02 14:45:20 +0000

    fix: address #4884 - Remove `packaging` as a runtime dependency of `opentelemetry-instrumentation-falcon`
    Closes #4884

--- instrumentation/opentelemetry-instrumentation-falcon/src/opentelemetry/instrumentation/falcon/__init__.py
+++ instrumentation/opentelemetry-instrumentation-falcon/src/opentelemetry/instrumentation/falcon/__init__.py
@@ -180,10 +180,10 @@ from timeit import default_timer
 from typing import Collection
 
 import falcon
-from packaging import version as package_version
 
 import opentelemetry.instrumentation.wsgi as otel_wsgi
 from opentelemetry import context, trace
+from packaging import version as package_version
 from opentelemetry.instrumentation._semconv import (
     HTTP_DURATION_HISTOGRAM_BUCKETS_NEW,
     _get_schema_url,
