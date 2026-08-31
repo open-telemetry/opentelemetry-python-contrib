d4fef1a548ab628781420c17445ba19fe01abc2f
henry3260 <henrychen@apache.org>
2026-08-31 19:11:29 +0800

    celery: re-enable the metrics uninstrument test on PyPy and add a changelog entry

--- instrumentation/opentelemetry-instrumentation-celery/tests/test_metrics.py
+++ instrumentation/opentelemetry-instrumentation-celery/tests/test_metrics.py
@@ -3,10 +3,7 @@
 
 import threading
 import time
-from platform import python_implementation
 from timeit import default_timer
-
-from pytest import mark
 
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from opentelemetry.test.test_base import TestBase
@@ -95,7 +92,6 @@ class TestMetrics(TestBase):
             est_value_delta=200,
         )
 
-    @mark.skipif(python_implementation() == "PyPy", reason="Fails randomly in pypy")
     def test_metric_uninstrument(self):
         CeleryInstrumentor().instrument()
 
